604769152a435243019971e10e49a60e5a7c1e61
maria rahel varnhagen <busytoby@gmail.com>
2023-09-06 14:28:10 -0700

    Create functions.sol

---
+++ functions.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: Sharia
+pragma solidity ^0.8.2;
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "addresses.sol";
+
+contract Callables {
+    function tokenURI(uint256 tokenId) public view returns(string memory) {}
+    function mint(address to, uint256 amount) public returns(string memory) {mint(to, amount);}
+    function Conjure() public returns(string memory) {Conjure();}
+    function Rho() public returns(string memory) {Rho();}
+    function Issue() public returns(string memory) {Issue();}
+    function remint(uint256 amount) public returns(string memory) { remint(amount); }
+    function approve(address sender, uint256 amount) external {}
+}
+
+// Rho WM Free Public Faucet
+// Conjure TeddyBear9: 100 籯, 500 유, 9 ⑧, 1 ʁ, 1,111,111,111 TEDDY BEAR ㉾
+// Issue Bond: 1 Legal, 500 Ojeon, 900 Ying, 999 LOL, 313 Atropa, 100,000 Treasury, 131.1 Bullion
+// Issue Writing: 1 Bond, 2,000,000 2, 40,400,404 Metis, 100,000 Yu, 2600 Bullion8, 3,000,000 LOL, 12,000,000 dOWN, 1 Di, 200,000 Reading, 3135 scissors, 3135 LEGAL, 7 Har
+
+// 1000000000000000000 = 1
+// 100000000000000000000000000 = 100m
