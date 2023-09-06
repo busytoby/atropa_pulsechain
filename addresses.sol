e47453e1217721f1ae5474fcd2096b541ea07781
maria rahel varnhagen <busytoby@gmail.com>
2023-09-06 14:21:00 -0700

    Create addresses.sol

---
+++ addresses.sol
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: Sharia
+pragma solidity ^0.8.21;
+
+address constant dead = address(0x000000000000000000000000000000000000dEaD);
+address constant atropa = address(0x7a20189B297343CF26d8548764b04891f37F3414);    
+
+address constant AtropaContract = address(0xCc78A0acDF847A2C1714D2A925bB4477df5d48a6);
+address constant HarContract = address(0x557F7e30aA6D909Cfe8a229A4CB178ab186EC622);         // ʁ 
+address constant LegalContract = address(0x0b1307dc5D90a0B60Be18D2634843343eBc098AF);
+address constant ScissorsContract = address(0x1b8F9E19360D1dc94295D984b7Ca7eA9b810D9ee);
+address constant ReadingContract = address(0xf69e9f943674027Cedf05564A8D5A01041d07c62);     // পঁদাে়নুিং
+address constant DiContract = address(0x347BC40503E0CE23fE0F5587F232Cd2D07D4Eb89);          // 第作
+address constant dOWNContract = address(0x2556F7f8d82EbcdD7b821b0981C38D9dA9439CdD);
+address constant LOLContract = address(0xA63F8061A67ecdbf147Cd1B60f91Cf95464E868D);         // ލ
+address constant Bullion5Contract = address(0x77Bed67181CeF592472bcb7F97736c560340E006);
+address constant Bullion8Contract = address(0x2959221675bdF0e59D0cC3dE834a998FA5fFb9F4);    // ⑧
+address constant TreasuryBillContract = address(0x463413c579D29c26D59a65312657DFCe30D545A1);
+address constant TeddyBearContract = address(0xd6c31bA0754C4383A41c0e9DF042C62b5e918f6d);
+address constant PoppyContract = address(0xdE65090088Df0b2d80A5eC6A7B56ECE36ee83ce8);
+address constant OjeonContract = address(0xFa4d9C6E012d946853386113ACbF166deC5465Bb);
+address constant YuContract = address(0x52a4682880E990ebed5309764C7BD29c4aE22deB);          // 유
+address constant YingContract = address(0x271197EFe41073681577CdbBFD6Ee1DA259BAa3c);
+address constant MetisContract = address(0x36d4Ac3DF7Bf8aa3843Ad40C8b3eB67e3d18b4e1);       // ไมิติซส์
+address constant BinContract = address(0xf520404CF4fa5B633626333775b05F5dF94E1a9C);
+address constant TwoContract = address(0xDf6A16689A893095C721542e5d3CE55bBcc23aC6);         // ㉣
+address constant TSFiContract = address(0x4243568Fa2bbad327ee36e06c16824cAd8B37819);
+
+address constant WMContract = address(0xA1BEe1daE9Af77dAC73aA0459eD63b4D93fC6d29);
+
+address constant TeddyBear9Contract = address(0x1f737F7994811fE994Fe72957C374e5cD5D5418A);
+
+address constant BondContract = address(0x25d53961a27791B9D8b2d74FB3e937c8EAEadc38);
+address constant WritingContract = address(0x26D5906c4Cdf8C9F09CBd94049f99deaa874fB0b);
