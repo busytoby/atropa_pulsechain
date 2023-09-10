d0014e1e61c20d945c2e3e28597a7db89f5b9d32
maria rahel varnhagen <busytoby@gmail.com>
2023-09-10 09:56:22 -0700

    Update functions.sol

--- functions.sol
+++ functions.sol
@@ -17,7 +17,7 @@ contract Callables {
 // Rho WM Free Public Faucet
 // Conjure TeddyBear9: 100 籯, 500 유, 9 ⑧, 1 ʁ, 1,111,111,111 TEDDY BEAR ㉾
 // Issue Bond: 1 Legal, 500 Ojeon, 900 Ying, 999 LOL, 313 Atropa, 100,000 Treasury, 131.1 Bullion
-// Issue Writing: 1 Bond, 2,000,000 2, 40,400,404 Metis, 100,000 Yu, 2600 Bullion8, 3,000,000 LOL, 12,000,000 dOWN, 1 Di, 200,000 Reading, 3135 scissors, 3135 LEGAL, 7 Har
+// // Issue Writing: 1 Bond, 2,000,000 2, 40,400,404 Metis, 100,000 Yu, 2600 Bullion8, 3,000,000 LOL, 12,000,000 dOWN, 1 Di, 200,000 Reading, 3135 scissors, 3135 LEGAL, 7 Har
 // Approve Method Is Strictly Less-Than So Be Sure To Approve For ex 40,400,405 Or At Least 40,400,404.00000000001 etc
 
 // 1000000000000000000 = 1
