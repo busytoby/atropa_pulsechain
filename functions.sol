55a224c493312d60a518374efdfc60fae1a53fef
maria rahel varnhagen <busytoby@gmail.com>
2023-09-13 16:24:36 -0700

    Update functions.sol

--- functions.sol
+++ functions.sol
@@ -11,12 +11,18 @@ contract MyContract {
     function MU() public returns(string memory) {MU();}         // Call
     function Rho() public returns(string memory) {Rho();}       // WM
     function Psi() public returns(string memory) {Psi();}       // WenTi
+    function nu() public returns(string memory) {nu();}         // Tristar and Red Sector Incorporated
+    //function NU() public returns(string memory) {NU();}         // Dial 666-ANNABELLE
     function Phi() public returns(string memory) {Phi();}       // hhFa
     function SIGMA() public returns(string memory) {SIGMA();}   // NoNukes
     function Theta() public returns(string memory) {Theta();}   // FA
-    function Issue() public returns(string memory) {Issue();}
+    function Issue() public returns(string memory) {Issue();} 
     function remint(uint256 amount) public returns(string memory) { remint(amount); }
 }
+
+// Conjure TeddyBear9: 100 籯, 500 유, 9 ⑧, 1 ʁ, 1,111,111,111 TEDDY BEAR ㉾
+// Issue Bond: 1 Legal, 500 Ojeon, 900 Ying, 999 LOL, 313 Atropa, 100,000 Treasury, 131.1 Bullion
+// Issue Writing: 1 Bond, 2,000,000 2, 40,400,404 Metis, 100,000 Yu, 2600 Bullion8, 3,000,000 LOL, 12,000,000 dOWN, 1 Di, 200,000 Reading, 3135 scissors, 3135 LEGAL, 7 Har
 
 // 1000000000000000000 = 1
 // 100000000000000000000000000 = 100m
