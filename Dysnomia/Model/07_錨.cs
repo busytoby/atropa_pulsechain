fc0f5d5ddd1bd706e7effb5d64830c18a6fa41be
maria <maria@twee.se>
2023-12-03 14:56:48 -0800

    Solving For S

--- Dysnomia/Model/07_錨.cs
+++ Dysnomia/Model/07_錨.cs
@@ -10,12 +10,28 @@ namespace Dysnomia
     public class 錨
     {
         public 锚 Mu;
+        public 锚 Rho;
+        public 锚 Psi;
+
+
+        private static void Anchor(ref 锚 Beta)
+        {
+            Beta = new 锚();
+        }
 
         public 錨()
         {
-            Mu = new 锚();
-            Mu.Pi();
+            Task t0 = new Task(() => { Anchor(ref Mu); Mu.Pi(); });
+            t0.Start();
+            Task t1 = new Task(() => { Anchor(ref Rho); Rho.Pi(); });
+            t1.Start();
+            Task t2 = new Task(() => { Anchor(ref Psi); Psi.Pi(); });
+            t2.Start();
 
+            int i = 0;
+            while(i++ < 999) {
+                System.Threading.Thread.Sleep(2000);
+            }
         }
     }
 }
