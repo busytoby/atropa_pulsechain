046c0c4c848ece3bc0ab302f3ce5e494718a86d4
maria <maria@twee.se>
2023-12-09 20:40:38 -0800

    OpCodes Again

--- viewclient/MainWindow.xaml.cs
+++ viewclient/MainWindow.xaml.cs
@@ -52,6 +52,8 @@ namespace Pulse
             while (A == null || Zuo.Mu == null | A.Rho == null || A.Psi == null || A.Nu == null)
                 Thread.Sleep(400);
 
+            OpCode Beta = new OpCode(A.Rho.Rho.Psi.Theta.Xi[0], "Atropa");
+
             int i = 99;
         }
 
