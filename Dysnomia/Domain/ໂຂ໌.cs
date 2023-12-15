08ba73ce55f3d3374a0102f57e0b0010138b416a
maria <maria@twee.se>
2023-12-15 14:05:03 -0800

    Use Last

--- Dysnomia/Domain/ໂຂ໌.cs
+++ Dysnomia/Domain/ໂຂ໌.cs
@@ -17,14 +17,14 @@ namespace Dysnomia.Domain
         public ໂຂ໌(錨 Beta)
         {
             Faung Iota = Beta.Nu.Rho.Psi.Theta.Xi[0];
-            Fa Omicron = Beta.Nu.Rho.Sigma.Rho[0];
+            Fa Omicron = Beta.Nu.Rho.Sigma.Rho.Last();
             Fa Lambda = Beta.Nu.Rho.Sigma.Pi(Beta.Nu.Rho.Psi.Rho.Element);
             Eta = new Faung(ref Omicron, ref Lambda);
             Sigma = new ည(ref Iota, ref Lambda, false);
             Iota = Beta.Nu.Rho.Psi.Theta.Xi[1];
             Upsilon = new ည(ref Iota, ref Beta.Nu.Rho.Psi.Theta.Chi, false);
             Tau = new ညြ(ref Zuo.Mu.Mu.Theta, ref Sigma, ref Upsilon);
-            Omicron = Beta.Psi.Rho.Sigma.Rho[0];
+            Omicron = Beta.Psi.Rho.Sigma.Rho.Last();
             Theta = new Faung(ref Omicron, ref Tau.Upsilon);
         }
     }
