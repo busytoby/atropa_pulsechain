5aa2c0f93c4a1c531fe77a61672f288309430e45
maria <maria@twee.se>
2023-12-27 12:49:43 -0800

    ..

--- Dysnomia/Domain/Buffer.cs
+++ Dysnomia/Domain/Buffer.cs
@@ -14,15 +14,26 @@ namespace Dysnomia.Domain
         public Buffer(Faung Mu, byte[] Rho) {
             int _size = Rho.Length;
             Bytes = new byte[_size];
+            Encode(Mu, Rho);
+        }
+
+        private void Encode(Faung Mu, byte[] Rho)
+        {
+            int _size = Rho.Length;
 
             BigInteger Beta = Mu.Rod.Mu(Mu.Rod.Kappa, Mu.Cone.Kappa, Mu.Pi);
-            for (int i = 0; i < _size;) {
+            for (int i = 0; i < _size;)
+            {
                 byte[] Iota = Beta.ToByteArray();
                 for (int j = 0; j < Iota.Length && i < _size; j++, i++)
                     Bytes[i] = (byte)(Iota[j] ^ Rho[i]);
                 Beta = Mu.Rod.Mu(Beta, Mu.Cone.Kappa, Mu.Pi);
             }
-            
+        }
+
+        public void Decode(Faung Mu)
+        {
+            Encode(Mu, Bytes);
         }
 
         public String ToString()
