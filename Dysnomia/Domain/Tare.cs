a4cc1712cd959b7c2713c588cf179593a40f4d0e
maria <maria@twee.se>
2023-12-28 09:15:16 -0800

    Generic callback based logging

---
+++ Dysnomia/Domain/Tare.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dysnomia.Domain
+{
+    static public class Tare
+    {
+        public delegate void Gram(byte[] A);
+        static public List<Gram> Grams;
+
+        static Tare()
+        {
+            Grams = new List<Gram>();
+        }
+
+        static public void AddCustom(Gram A)
+        {
+            Grams.Add(A);
+        }
+
+        static public void Log(byte[] A)
+        {
+            foreach (Gram G in Grams) G(A);
+        }
+    }
+}
