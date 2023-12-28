41eaa7a5b916271b43b958bd66d217f1cd5fbe5c
maria <maria@twee.se>
2023-12-28 09:40:27 -0800

    cleanup

--- viewclient/App.xaml.cs
+++ viewclient/App.xaml.cs
@@ -13,8 +13,6 @@ namespace Pulse
     /// </summary>
     public partial class App : Application
     {
-        //private 錨 A;
-
         private void Bootstrap(object sender, StartupEventArgs e)
         {
             ShowApparitionWindow();
@@ -36,16 +34,6 @@ namespace Pulse
             O.Reset();
             A = O.Encode("Test");
             B = O.Encode("Test");
-
-            //Task t0 = new Task(() => { A = new 錨(); });
-            //t0.Start();
-
-            //Pulse.API Comptroller = new Pulse.API();
-            //InitializeComponent();
-            //StartThreads();
-
-            //Task t4 = new Task(() => { Beta(); });
-            //t4.Start();
         }
 
         private void ShowApparitionWindow()
@@ -61,32 +49,6 @@ namespace Pulse
             Logging.Add(Dysnomia.Apparition.Input);
             Logging.Log("Ready");
         }
-
-        /*
-        private void Beta()
-        {
-            while (A == null || Zuo.Mu == null | A.Rho == null || A.Psi == null || A.Nu == null)
-                Thread.Sleep(400);
-
-            List<ည> B = Zuo.Mu.Nu;
-            ໂຂ໌ C = A.Beta();
-            if (Dysnomia.Apparition.Stub != true) throw new Exception("Zero Apparition");
-
-            Application.Current.Dispatcher.Invoke((Action)delegate
-            {
-                Dysnomia.Apparition.Window = new Apparition.Retaliation.RetaliationWindow();
-                Dysnomia.Apparition.Window.Show();
-            });
-
-            Dysnomia.Apparition.Tau.WaitOne();
-            Dysnomia.Apparition.MsgQueue.Enqueue(new Tuple<String, String>("Test", "Ready"));
-            Dysnomia.Apparition.Tau.ReleaseMutex();
-            Dysnomia.Apparition.Tau.WaitOne();
-            Dysnomia.Apparition.MsgQueue.Enqueue(new Tuple<String, String>("Test", "Two"));
-            Dysnomia.Apparition.MsgQueue.Enqueue(new Tuple<String, String>("Ready", "Three"));
-            Dysnomia.Apparition.Tau.ReleaseMutex();
-        }
-        */
     }
 
 }
