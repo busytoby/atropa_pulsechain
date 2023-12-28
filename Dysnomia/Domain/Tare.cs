bbca4a1947c0601a3d1d0838af9b4d6d79930ee8
maria <maria@twee.se>
2023-12-28 09:44:25 -0800

    cleanup

--- Dysnomia/Domain/Tare.cs
+++ Dysnomia/Domain/Tare.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using static Dysnomia.Domain.Tare;
-
+﻿using static Dysnomia.Domain.Tare;
 namespace Dysnomia.Domain
 {
     public class Tare : List<Gram>
