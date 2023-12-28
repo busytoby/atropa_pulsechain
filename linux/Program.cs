b55cb7b223a2c78b96cf982c459bfee8ea49b990
maria <busytoby@gmail.com>
2023-12-28 22:08:58 +0000

    runs on linux

---
+++ linux/Program.cs
@@ -0,0 +1,18 @@
+﻿using Dysnomia.Domain;
+using Dysnomia.Domain.World;
+using System.Text;
+
+// Edit ../Dysnomia/Dysnomia.csproj to remove the WPF line & change dotnet8-windows to dotnet8
+namespace Pulse {
+    public class ConsoleApp {
+	static void Main(string[] args) {
+	    Logging.Add(Input);
+            Oracle O = new Oracle();
+	}
+	    
+	public static void Input(Tare.MSG A) {
+            Console.WriteLine("<" + Encoding.Default.GetString(A.From) + "> " + Encoding.Default.GetString(A.Data));
+	}
+    }
+}
+
