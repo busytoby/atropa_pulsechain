3372037af62077065950f25968a32c6b46e579a9
maria <maria@twee.se>
2024-01-01 15:36:25 -0800

    ..

--- Dysnomia/Domain/World/Greed.cs
+++ Dysnomia/Domain/World/Greed.cs
@@ -78,7 +78,7 @@ namespace Dysnomia.Domain.World
                         int A, B;
                         for (int i = A = B = 0; i < size; i++)
                         {
-                            if (i == 0 && Omicron[0] != 0x10) B = -1;
+                            if (i == A && Omicron[A] != 0x10) B = -1;
                             if (i >= A && Omicron.Slice(i, 4).SequenceEqual<Byte>(Encoding.Default.GetBytes(Fi.DLE)))
                             {
                                 if (B == 0) A = i + 4;
