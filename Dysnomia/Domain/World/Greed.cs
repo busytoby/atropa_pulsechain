90ed0a5db66b45cc7c6038df50a060615b6ee5bc
maria <maria@twee.se>
2024-01-01 14:51:51 -0800

    probably unhandled client disconnect

--- Dysnomia/Domain/World/Greed.cs
+++ Dysnomia/Domain/World/Greed.cs
@@ -54,130 +54,137 @@ namespace Dysnomia.Domain.World
 
             while (Mu.Connected)
             {
-                while(Theta != null && Theta.In != null && Theta.In.Count > 0)
+                try
                 {
-                    if (!Theta.In.TryDequeue(out Lambda)) throw new Exception("Cannot Dequeue");
-                    String Subject = Encoding.Default.GetString(Lambda.Subject);
-                    if (Cone && Subject == "Xi")
+                    while (Theta.In.Count > 0)
                     {
-                        BigInteger Delta = new BigInteger(Lambda.Data);
-                        Rho.Tau = Rho.Avail(Delta);
-                        Handshake("Tau", Rho.Tau);
-                    }
-                    else throw new Exception("Unknown Handshake Subject");
-                }
-
-                if (Iota.DataAvailable)
-                {
-                    Thread.Sleep(200);
-                    int size = Iota.Read(Omicron);
-                    Queue<byte[]> Candidates = new Queue<byte[]>();
-
-                    int A, B;
-                    for (int i = A = B = 0; i < size; i++)
-                    {
-                        if (i == 0 && Omicron[0] != 0x10) B = -1;
-                        if (i >= A && Omicron.Slice(i, 4).SequenceEqual<Byte>(Encoding.Default.GetBytes(Fi.DLE)))
+                        if (!Theta.In.TryDequeue(out Lambda)) throw new Exception("Cannot Dequeue");
+                        String Subject = Encoding.Default.GetString(Lambda.Subject);
+                        if (Cone && Subject == "Xi")
                         {
-                            if(B == 0) A = i + 4;
-                            B = i - A;
+                            BigInteger Delta = new BigInteger(Lambda.Data);
+                            Rho.Tau = Rho.Avail(Delta);
+                            Handshake("Tau", Rho.Tau);
                         }
-                        if (B <= 0) continue;
+                        else throw new Exception("Unknown Handshake Subject");
+                    }
 
-                        Span<Byte> Token = Omicron.Slice(A, B);
+                    if (Iota.DataAvailable)
+                    {
+                        Thread.Sleep(200);
+                        int size = Iota.Read(Omicron);
+                        Queue<byte[]> Candidates = new Queue<byte[]>();
 
-                        if (Cone)
+                        int A, B;
+                        for (int i = A = B = 0; i < size; i++)
                         {
-                            if (Rho.Pole.IsZero && PeerChannel.IsZero)
+                            if (i == 0 && Omicron[0] != 0x10) B = -1;
+                            if (i >= A && Omicron.Slice(i, 4).SequenceEqual<Byte>(Encoding.Default.GetBytes(Fi.DLE)))
                             {
-                                BigInteger Alpha = new BigInteger(Token);
-                                Rho.Form(Alpha);
-                                Rho.Polarize();
-                                Handshake("Pole", Rho.Pole);
+                                if (B == 0) A = i + 4;
+                                B = i - A;
                             }
-                            else if (Rho.Coordinate.IsZero)
+                            if (B <= 0) continue;
+
+                            Span<Byte> Token = Omicron.Slice(A, B);
+
+                            if (Cone)
                             {
-                                BigInteger Pole = new BigInteger(Token);
-                                Rho.Conjugate(ref Pole);
-                                Rho.Conify();
-                                Handshake("Foundation", Rho.Foundation);
-                                Handshake("Channel", Rho.Channel);
-                            }
-                            else if(Rho.Element.IsZero && PeerFoundation.IsZero)
-                            {
-                                PeerFoundation = new BigInteger(Token);
-                            }
-                            else if (Rho.Element.IsZero && PeerChannel.IsZero)
-                            {
-                                PeerChannel = new BigInteger(Token);
-                                Rho.Saturate(PeerFoundation, PeerChannel);
-                                Rho.Bond();
-                                Handshake("Dynamo", Rho.Dynamo);
-                            }
-                            else if (Rho.Ring.IsZero)
-                            {
-                                PeerDynamo = new BigInteger(Token);
-                                Rho.Adduct(PeerDynamo);
-                                Rho.Open();
-                                Logging.Log("Greed", "Cone Handshake Complete: " + Rho.Barn, 2);
+                                if (Rho.Pole.IsZero && PeerChannel.IsZero)
+                                {
+                                    BigInteger Alpha = new BigInteger(Token);
+                                    Rho.Form(Alpha);
+                                    Rho.Polarize();
+                                    Handshake("Pole", Rho.Pole);
+                                }
+                                else if (Rho.Coordinate.IsZero)
+                                {
+                                    BigInteger Pole = new BigInteger(Token);
+                                    Rho.Conjugate(ref Pole);
+                                    Rho.Conify();
+                                    Handshake("Foundation", Rho.Foundation);
+                                    Handshake("Channel", Rho.Channel);
+                                }
+                                else if (Rho.Element.IsZero && PeerFoundation.IsZero)
+                                {
+                                    PeerFoundation = new BigInteger(Token);
+                                }
+                                else if (Rho.Element.IsZero && PeerChannel.IsZero)
+                                {
+                                    PeerChannel = new BigInteger(Token);
+                                    Rho.Saturate(PeerFoundation, PeerChannel);
+                                    Rho.Bond();
+                                    Handshake("Dynamo", Rho.Dynamo);
+                                }
+                                else if (Rho.Ring.IsZero)
+                                {
+                                    PeerDynamo = new BigInteger(Token);
+                                    Rho.Adduct(PeerDynamo);
+                                    Rho.Open();
+                                    Logging.Log("Greed", "Cone Handshake Complete: " + Rho.Barn, 2);
+                                }
+                                else
+                                    throw new Exception("Not Implemented");
                             }
                             else
-                                throw new Exception("Not Implemented");
-                        }
-                        else
-                        {
-                            if (Rho.Alpha.IsZero)
                             {
-                                BigInteger Xi = new BigInteger(Token);
-                                Rho.Alpha = Rho.Avail(Xi);
-                                Handshake("Alpha", Rho.Alpha);
-                            } else if(Rho.Pole.IsZero && PeerChannel.IsZero)
-                            {
-                                BigInteger Tau = new BigInteger(Token);
-                                Rho.Form(Tau);
-                                Rho.Polarize();
-                                Handshake("Pole", Rho.Pole);
-                            } else if(Rho.Coordinate.IsZero)
-                            {
-                                BigInteger Pole = new BigInteger(Token);
-                                Rho.Conjugate(ref Pole);
-                            } else if(Rho.Element.IsZero && PeerFoundation.IsZero)
-                            {
-                                PeerFoundation = new BigInteger(Token);
-                            } else if(Rho.Element.IsZero && PeerChannel.IsZero)
-                            {
-                                PeerChannel = new BigInteger(Token);
-                                Rho.Saturate(PeerFoundation, PeerChannel);
-                                Rho.Bond();
-                                Handshake("Foundation", Rho.Foundation);
-                                Handshake("Channel", Rho.Channel);
-                                Handshake("Dynamo", Rho.Dynamo);
+                                if (Rho.Alpha.IsZero)
+                                {
+                                    BigInteger Xi = new BigInteger(Token);
+                                    Rho.Alpha = Rho.Avail(Xi);
+                                    Handshake("Alpha", Rho.Alpha);
+                                }
+                                else if (Rho.Pole.IsZero && PeerChannel.IsZero)
+                                {
+                                    BigInteger Tau = new BigInteger(Token);
+                                    Rho.Form(Tau);
+                                    Rho.Polarize();
+                                    Handshake("Pole", Rho.Pole);
+                                }
+                                else if (Rho.Coordinate.IsZero)
+                                {
+                                    BigInteger Pole = new BigInteger(Token);
+                                    Rho.Conjugate(ref Pole);
+                                }
+                                else if (Rho.Element.IsZero && PeerFoundation.IsZero)
+                                {
+                                    PeerFoundation = new BigInteger(Token);
+                                }
+                                else if (Rho.Element.IsZero && PeerChannel.IsZero)
+                                {
+                                    PeerChannel = new BigInteger(Token);
+                                    Rho.Saturate(PeerFoundation, PeerChannel);
+                                    Rho.Bond();
+                                    Handshake("Foundation", Rho.Foundation);
+                                    Handshake("Channel", Rho.Channel);
+                                    Handshake("Dynamo", Rho.Dynamo);
+                                }
+                                else if (Rho.Ring.IsZero)
+                                {
+                                    PeerDynamo = new BigInteger(Token);
+                                    Rho.Adduct(PeerDynamo);
+                                    Rho.Open();
+                                    Logging.Log("Greed", "Rod Handshake Complete: " + Rho.Barn, 2);
+                                }
+                                else
+                                    throw new Exception("Not Implemented");
                             }
-                            else if(Rho.Ring.IsZero)
-                            {
-                                PeerDynamo = new BigInteger(Token);
-                                Rho.Adduct(PeerDynamo);
-                                Rho.Open();
-                                Logging.Log("Greed", "Rod Handshake Complete: " + Rho.Barn, 2);
-                            }
-                            else
-                                throw new Exception("Not Implemented");
-                        }
 
-                        A = i + 4;
-                        B = 0;
-                        i += 3;
+                            A = i + 4;
+                            B = 0;
+                            i += 3;
+                        }
+                        Omicron.Clear();
                     }
-                    Omicron.Clear();
-                }
-               
-                while(Theta.Out.Count > 0)
-                {
-                    if (!Theta.Out.TryDequeue(out Lambda)) throw new Exception("Cannot Dequeue");
-                    Iota.Write(Encoding.Default.GetBytes(Fi.DLE));
-                    Iota.Write(Lambda.Data);
-                    Iota.Write(Encoding.Default.GetBytes(Fi.DLE));
-                }
+
+                    while (Theta.Out.Count > 0)
+                    {
+                        if (!Theta.Out.TryDequeue(out Lambda)) throw new Exception("Cannot Dequeue");
+                        Iota.Write(Encoding.Default.GetBytes(Fi.DLE));
+                        Iota.Write(Lambda.Data);
+                        Iota.Write(Encoding.Default.GetBytes(Fi.DLE));
+                    }
+                } catch (Exception E) { }
             }
         }
     }
