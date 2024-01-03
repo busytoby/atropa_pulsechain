3ea9edd0e1908be9385471c382480229181852df
maria <maria@twee.se>
2024-01-03 15:57:26 -0800

    bug

--- Dysnomia/Domain/bin/connect.cs
+++ Dysnomia/Domain/bin/connect.cs
@@ -16,11 +16,12 @@ namespace Dysnomia.Domain.bin
         protected override void Phi()
         {
             byte[] From = Encoding.Default.GetBytes(Name);
-            Theta.In.Enqueue(new Tare.MSG(From, new byte[] { 0x05 }, 6));
+
             if (Args.Length == 0)
                 Theta.Out.Enqueue(new Tare.MSG(From, Encoding.Default.GetBytes("Connect Command Requires At Least 1 Argument"), 6));
             else
             {
+                Theta.In.Enqueue(new Tare.MSG(From, new byte[] { 0x05 }, 6));
                 Theta.In.Enqueue(new Tare.MSG(From, Encoding.Default.GetBytes(Args[0]), 6));
                 if(Args.Length < 2) Theta.In.Enqueue(new Tare.MSG(From, BitConverter.GetBytes(Int16.Parse("5555")), 6)); // 0x15B3 = 5555
                 else Theta.In.Enqueue(new Tare.MSG(From, BitConverter.GetBytes(Int16.Parse(Args[1])), 6));
