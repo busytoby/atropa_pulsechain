792bc28e8c00640186d9d938b02172cbde49dc7f
maria <maria@twee.se>
2024-01-09 09:26:16 -0800

    Dysnomia Time Extension Uses A 34 Hour 100 Minute Per Day Clock And Starts At Time Zero Of 638403877000000000 Ticks

--- Dysnomia/Lib/ExtensionMethods.cs
+++ Dysnomia/Lib/ExtensionMethods.cs
@@ -12,6 +12,24 @@ namespace ExtensionMethods
 {
     public static class ExtensionMethods
     {
+        public struct DysnomiaTime {
+            public static readonly long Zero = 638403877000000000;
+            public static readonly long TicksPerHour = TimeSpan.TicksPerDay / 34;
+            public static readonly long TicksPerMinute = TimeSpan.TicksPerHour / 100;
+
+            public int Day { get; }
+            public int Hour { get; }
+            public int Minute { get; }
+
+            public DysnomiaTime(DateTime T)
+            {
+                DateTime DTNow = new DateTime(T.Ticks - Zero);
+                Day = Convert.ToInt32(DTNow.Ticks / TimeSpan.TicksPerDay);
+                Hour = Convert.ToInt32((DTNow.Ticks - (TimeSpan.TicksPerDay * Day)) / TicksPerHour);
+                Minute = Convert.ToInt32((DTNow.Ticks - (TimeSpan.TicksPerDay * Day) - (TicksPerHour * Hour)) / TicksPerMinute);
+            }
+        }
+
         public static Serialization OpenSerialization(this Fa Rho)
         {
             Serialization Nu = new Serialization();
@@ -22,6 +40,11 @@ namespace ExtensionMethods
             Nu.Enqueue(Rho.Barn.ToByteArray());
             Nu.Enqueue(Rho.Element.ToByteArray());
             return Nu;
+        }
+
+        public static DysnomiaTime ToDysnomia(this DateTime Mu)
+        {
+            return new DysnomiaTime(Mu);
         }
     }
 }
--- viewclient/App.xaml.cs
+++ viewclient/App.xaml.cs
@@ -1,10 +1,12 @@
 ﻿using Dysnomia;
 using Dysnomia.Domain;
 using Dysnomia.Lib;
+using ExtensionMethods;
 using System.Configuration;
 using System.Data;
 using System.Text;
 using System.Windows;
+using static ExtensionMethods.ExtensionMethods;
 
 namespace Pulse
 {
@@ -19,6 +21,7 @@ namespace Pulse
             Logging.Add(Dysnomia.Apparition.Input);
             Dysnomia.Apparition.Interpreter = Controller.Oracle.ProcessString;
             Controller.Fi.Listen(5555);
+            DysnomiaTime T = DateTime.Now.ToDysnomia();
         }
 
         private void ShowApparitionWindow()
