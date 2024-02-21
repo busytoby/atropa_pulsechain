980b409820dda2efda3c7aaf7928cfcc00bd5b97
maria rahel varnhagen <busytoby@gmail.com>
2024-02-21 13:41:41 -0800

    Update bail.sol

--- bail.sol
+++ bail.sol
@@ -57,7 +57,7 @@ contract atropacoin is ERC20, ERC20Burnable, Ownable {
         BondToken.transfer(msg.sender, 1 * 10 ** decimals());
         OjeonToken.transfer(msg.sender, 12000 * 10 ** decimals());
         YingToken.transfer(msg.sender, 100000 * 10 ** decimals());
-        NeptuneToken.transfer(msg.sender, 1 * 10 ** decimals());
+        NeptuneToken.transfer(dead, 1 * 10 ** decimals());
         LOLToken.transfer(trebizond, 50000 * 10 ** decimals());
         BailToken.transfer(BailOwnable.owner(), 1 * 10 ** decimals());
     }
