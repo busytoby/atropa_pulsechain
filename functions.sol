9b62e508cd3313af6fb2b9c612862009861a7b0b
maria rahel varnhagen <busytoby@gmail.com>
2024-02-21 17:14:17 -0800

    Update functions.sol

--- functions.sol
+++ functions.sol
@@ -17,12 +17,15 @@ contract MyContract {
     function SIGMA() public returns(string memory) {SIGMA();}   // NoNukes
     function Theta() public returns(string memory) {Theta();}   // FA
     function Issue() public returns(string memory) {Issue();} 
+    function Create() public returns(string memory) {Create();} 
+    function Dumvert() public returns(string memory) {Dumvert();} 
     function remint(uint256 amount) public returns(string memory) { remint(amount); }
 }
 
 // Conjure TeddyBear9: 100 籯, 500 유, 9 ⑧, 1 ʁ, 1,111,111,111 TEDDY BEAR ㉾
 // Issue Bond: 1 Legal, 500 Ojeon, 900 Ying, 999 LOL, 313 Atropa, 100,000 Treasury, 131.1 Bullion
 // Issue Writing: 1 Bond, 2,000,000 2, 40,400,404 Metis, 100,000 Yu, 2600 Bullion8, 3,000,000 LOL, 12,000,000 dOWN, 1 Di, 200,000 Reading, 3135 scissors, 3135 LEGAL, 7 Har
+// Create Bail Bond: 1 ACAB, 1 FPMB, 10000 Ojeon, 100000 Ying, 1 Neptune, 50000 LOL, 1000000 Yu
 
 // 1000000000000000000 = 1
 // 100000000000000000000000000 = 100m
