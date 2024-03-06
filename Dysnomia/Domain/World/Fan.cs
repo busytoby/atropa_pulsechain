c90063a950534041b4fb3b4a127e0585d9ff0f4e
maria <maria@twee.se>
2024-03-05 17:10:17 -0800

    nm

--- Dysnomia/Domain/World/Fan.cs
+++ Dysnomia/Domain/World/Fan.cs
@@ -26,13 +26,6 @@ namespace Dysnomia.Domain.World
             Add(0, Proxy);
         }
 
-        public Fan(BigInteger ClientId, BigInteger Proxy)
-        {
-            Items = new List<Fang>();
-            Indexes = new Dictionary<BigInteger, int>();
-            Add(ClientId, Proxy);
-        }
-
         public void Add(BigInteger clientId, BigInteger Proxy)
         {
             Indexes.Add(clientId, Items.Count);
