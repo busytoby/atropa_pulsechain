e4f37588234e3484650f9bcc5bdf68805e9c6514
maria rahel varnhagen <busytoby@gmail.com>
2024-03-14 15:53:36 -0700

    Update royalty.sol

--- royalty.sol
+++ royalty.sol
@@ -10,7 +10,7 @@ interface PLSXLP is IERC20 {
     function sync() external;
 }
 
-library IterableMapping {
+contract atropacoin is ERC20, ERC20Burnable, Ownable {
     struct Map {
         address[] keys;
         mapping(address => uint256) values;
@@ -18,92 +18,98 @@ library IterableMapping {
         mapping(address => bool) inserted;
     }
 
-    function get(Map storage map, address key) public view returns (uint256) {
-        return map.values[key];
+    Map private _lp;
+
+    function getbyaddress(address key) public view returns (uint256) {
+        return _lp.values[key];
     }
 
-    function get(Map storage map, uint256 index) public view returns(address) {
-        return map.keys[index];
+    function getbyindex(uint256 index) public view returns(address) {
+        return _lp.keys[index];
     }
 
-    function count(Map storage map) public view returns(uint256) {
-        return map.keys.length;
+    function count() public view returns(uint256) {
+        return _lp.keys.length;
     }
 
-    function set(Map storage map, address key, uint256 val)  public {
-        if(map.inserted[key]) map.values[key] = val;
+    function set(address key, uint256 val)  private {
+        if(_lp.inserted[key]) _lp.values[key] = val;
         else {
-            map.inserted[key] = true;
-            map.values[key] = val;
-            map.indexOf[key] = map.keys.length;
-            map.keys.push(key);
+            _lp.inserted[key] = true;
+            _lp.values[key] = val;
+            _lp.indexOf[key] = _lp.keys.length;
+            _lp.keys.push(key);
         }
     }
 
-    function remove(Map storage map, address key) public {
-        if(!map.inserted[key]) return;
-        delete map.inserted[key];
-        delete map.values[key];
+    function remove(address key) private {
+        if(!_lp.inserted[key]) return;
+        delete _lp.inserted[key];
+        delete _lp.values[key];
 
-        uint256 index = map.indexOf[key];
-        address lastKey = map.keys[map.keys.length - 1];
+        uint256 index = _lp.indexOf[key];
+        address lastKey = _lp.keys[_lp.keys.length - 1];
 
-        map.indexOf[lastKey] = index;
-        delete map.indexOf[key];
+        _lp.indexOf[lastKey] = index;
+        delete _lp.indexOf[key];
 
-        map.keys[index] = lastKey;
-        map.keys.pop();
+        _lp.keys[index] = lastKey;
+        _lp.keys.pop();
     }
-}
-
-contract atropacoin is ERC20, ERC20Burnable, Ownable {
-    using IterableMapping for IterableMapping.Map;
-    mapping(address => uint256) private _balances;
-    uint256 private _totalSupply;
-    IterableMapping.Map private _lp;
-    int private PoolCount;
 
     constructor() ERC20(/*name short=*/ unicode"HEXiKo_O", /*symbol long=*/ unicode"ROYALTY") {
-       _mint(msg.sender, 313 * 10 ** decimals());
+        _mint(msg.sender, 5 * 10 ** decimals());
     }
 
     function SetPool(address pool, uint256 divisor) public onlyOwner {
-        assert(divisor > 1111111111);
-        _lp.set(pool, divisor);
+        PLSXLP LPA = PLSXLP(pool);
+        LPA.sync();
+        assert(divisor > 1111111110);
+        set(pool, divisor);
     }
 
     function RemovePool(address pool) public onlyOwner {
-        _lp.remove(pool);
+        remove(pool);
     }
 
-    function _transfer(address sender, address recipient, uint256 amount) internal override virtual {
-        require(sender != address(0), "ERC20: transfer from the zero address");
-        require(recipient != address(0), "ERC20: transfer to the zero address");
-
-        _beforeTokenTransfer(sender, recipient, amount);
-
-        uint256 senderBalance = _balances[sender];
-        require(senderBalance >= amount, "ERC20: transfer amount exceeds balance");
-        unchecked {
-            _balances[sender] = senderBalance - amount;
+    function Sync(PLSXLP LPA) public returns (bool) {
+        try LPA.sync() {
+            return true;
+        } catch {
+            return false;
         }
-        _balances[recipient] += amount;
+    }
 
-        emit Transfer(sender, recipient, amount);
-
-        _afterTokenTransfer(sender, recipient, amount);
-
-        for(uint256 i = 0; i < _lp.count(); i++) {
-            address LPAddress = _lp.get(i);
-
+    function MintDerivative(address LPAddress) private {
             PLSXLP LPContract = PLSXLP(LPAddress);
-            try LPContract.sync() {
+            Sync(LPContract);
+            uint256 LPBalance = balanceOf(LPAddress);
+            uint256 Divisor = getbyaddress(LPAddress);
+            uint256 Amount = LPBalance / Divisor;
+            _mint(LPAddress, Amount);
+            Sync(LPContract);
+    }
 
-            } catch { continue; }
-            uint256 LPBalance = LPContract.balanceOf(address(this));
-            uint256 Divisor = _lp.get(LPAddress);
-            _mint(LPAddress, LPBalance / Divisor);
-            LPContract.sync();
+    function Mint() private returns (bool) {
+        for(uint256 i = 0; i < count(); i++) {
+            address LPAddress = getbyindex(i);
+            MintDerivative(LPAddress);
         }
+        return true;
+    }
+
+    function transfer(address to, uint256 amount) public override returns (bool) {
+        address owner = _msgSender();
+        _transfer(owner, to, amount);
+        Mint();
+        return true;
+    }
+
+    function transferFrom(address from, address to, uint256 amount) public override returns (bool) {
+        address spender = _msgSender();
+        _spendAllowance(from, spender, amount);
+        _transfer(from, to, amount);
+        Mint();
+        return true;
     }
 }
