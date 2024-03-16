9b15bd2f81a38c76b1b9b538774a46f4689c34c7
maria rahel varnhagen <busytoby@gmail.com>
2024-03-16 15:10:37 -0700

    Update royalty.sol

--- royalty.sol
+++ royalty.sol
@@ -50,7 +50,7 @@ contract atropacoin is Incorporation, Whitelist {
                 else
                     MintCAPS(Distribution);
                 if(Article.Class != Incorporation.Type.EXCHANGE && Article.Class != Incorporation.Type.FUTURE && Article.Class != Incorporation.Type.CAP)
-                Asset.Sync(LPAddress);
+                    Asset.Sync(LPAddress);
             }
         }
         
