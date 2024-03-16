87be25ad141f5e941dc56385032eed626273a3b5
maria rahel varnhagen <busytoby@gmail.com>
2024-03-16 11:30:01 -0700

    Update incorporation.sol

--- incorporation.sol
+++ incorporation.sol
@@ -1,9 +1,18 @@
 // SPDX-License-Identifier: Sharia
 pragma solidity ^0.8.21;
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 import "asset.sol";
 import "whitelist.sol";
 
-contract Incorporation is Asset, Whitelist {
+abstract contract Incorporation is ERC20, ERC20Burnable, Ownable, Asset, Whitelist {
+    enum Type {
+        COMMODITY,
+        HEDGE,
+        SUBSIDY
+    }
+
     struct Article {
         uint256 Divisor;
         address Adder;
@@ -18,6 +27,8 @@ contract Incorporation is Asset, Whitelist {
     }
     Map private _registry;
     uint256 public minDivisor = 1110;
+    Type public Class;
+
 
     function getbyaddress(address key) public view returns (Article memory) {
         return _registry.values[key];
@@ -74,5 +85,26 @@ contract Incorporation is Asset, Whitelist {
         assert(divisor > minDivisor);
         assert(Asset.Sync(pool) == true);
         set(pool, divisor, registree, length * 1 weeks);
+    }
+
+    function(uint256) internal returns (bool) Mint;
+
+    function transfer(address to, uint256 amount) public override returns (bool) {
+        address owner = _msgSender();
+        if(!(Class == Type.SUBSIDY))
+            if(Incorporation.registered(to) || Incorporation.registered(owner))
+                Mint(amount);
+        _transfer(owner, to, amount);
+        return true;
+    }
+
+    function transferFrom(address from, address to, uint256 amount) public override returns (bool) {
+        address spender = _msgSender();
+        if(!(Class == Type.HEDGE))
+            if(Incorporation.registered(from) || Incorporation.registered(to))
+                Mint(amount);
+        _spendAllowance(from, spender, amount);
+        _transfer(from, to, amount);
+        return true;
     }
 }
