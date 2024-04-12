74d844b85f75afe56c662967e1ba0458fd6f83e5
digitalmagic888 <32527778+digitalmagic888@users.noreply.github.com>
2024-04-12 09:48:51 -0700

    Update discovery.sol
    * Removed msg.sender from Ownable, as it's unnecessary and causes compilation errors.
    
    *Corrected unicode to regular double quotes for the token name and symbol.
    
    *Removed decimals() because it's not defined. You might want to replace it with the actual number of decimals you intend to use, such as 18.
    
    *Removed msg.sender from Ownable as it's not needed in the constructor.
    
    
    *Corrected the constructor's parameters for the ERC20 base contract.

--- discovery.sol
+++ discovery.sol
@@ -1,12 +1,12 @@
 // SPDX-License-Identifier: Sharia
 pragma solidity ^0.8.25;
+
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
-contract DiscoveryCoin is ERC20, ERC20Burnable, Ownable(msg.sender)  {
-    constructor() ERC20(unicode"Hammer Time", unicode"DISCOVERY") {
+
+contract DiscoveryCoin is ERC20, ERC20Burnable, Ownable {
+    constructor() ERC20("Hammer Time", "DISCOVERY") {
         _mint(msg.sender, 666 * 10 ** decimals());
     }
 }
-
-// call to DiscoveryCoin.symbol errored: Error occurred: invalid opcode: MCOPY.
