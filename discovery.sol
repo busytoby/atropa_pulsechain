2e8643ebcd384714505205f06c4deff1efc111e7
busytoby <maria@twee.se>
2024-04-12 10:55:51 -0700

    still not working

--- discovery.sol
+++ discovery.sol
@@ -1,10 +1,10 @@
 // SPDX-License-Identifier: Sharia
-pragma solidity ^0.8.25;
+pragma solidity ^0.8.20;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
-contract DiscoveryCoin is ERC20, ERC20Burnable, Ownable(msg.sender)  {
-    constructor() ERC20(unicode"Hammer Time", unicode"DISCOVERY") {
+contract DiscoveryCoin is ERC20, ERC20Burnable, Ownable  {
+    constructor() ERC20(unicode"Hammer Time", unicode"DISCOVERY") Ownable(msg.sender)  {
         _mint(msg.sender, 666 * 10 ** decimals());
     }
 }
