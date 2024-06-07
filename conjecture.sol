cde616ba3292e2c197d3d8987ca472548d3c3d6f
busytoby <maria@twee.se>
2024-06-07 11:46:33 -0700

    Xiao aa

--- conjecture.sol
+++ conjecture.sol
@@ -26,13 +26,13 @@ abstract contract Conjecture is Feh {
     }
 
     function Seed(Fa storage Rod) internal {
-        Rod.Base = aa.Random();
-        Rod.Secret = aa.Random();
-        Rod.Signal = aa.Random();
+        Rod.Base = Xiao.Random();
+        Rod.Secret = Xiao.Random();
+        Rod.Signal = Xiao.Random();
     }
 
    function Tune(Fa storage Rod) internal {
-        Rod.Channel = aa.modExp64(Rod.Base, Rod.Signal, MotzkinPrime);   
+        Rod.Channel = Xiao.modExp64(Rod.Base, Rod.Signal, MotzkinPrime);   
     }
 
     function Fuse(Fa storage Rod, uint64 _rho, uint64 Upsilon, uint64 Ohm) internal {
@@ -42,27 +42,27 @@ abstract contract Conjecture is Feh {
     }
 
     function Avail(Fa storage Rod, uint64 Xi) internal {
-        Rod.Alpha = aa.modExp64(Xi, Rod.Secret, MotzkinPrime);
+        Rod.Alpha = Xiao.modExp64(Xi, Rod.Secret, MotzkinPrime);
     }
 
     function Form(Fa storage Rod, uint64 Chi) internal {
-        Rod.Base = aa.modExp64(Chi, Rod.Secret, MotzkinPrime);
+        Rod.Base = Xiao.modExp64(Chi, Rod.Secret, MotzkinPrime);
         Tune(Rod);
     }
 
     function Polarize(Fa storage Rod) internal {
-        Rod.Pole = aa.modExp64(Rod.Base, Rod.Secret, MotzkinPrime);
+        Rod.Pole = Xiao.modExp64(Rod.Base, Rod.Secret, MotzkinPrime);
     }
 
     function Conjugate(Fa storage Rod, uint64 Chi) internal {
-        Rod.Coordinate = aa.modExp64(Chi, Rod.Secret, MotzkinPrime);
+        Rod.Coordinate = Xiao.modExp64(Chi, Rod.Secret, MotzkinPrime);
         // Chi = 0;
     }
 
     function Conify(Fa storage Rod, uint64 _Beta) internal {
         assert(Rod.Nu == 0);
         Rod.Identity = _Beta;
-        Rod.Foundation = aa.modExp64(Rod.Base, Rod.Identity, MotzkinPrime);
+        Rod.Foundation = Xiao.modExp64(Rod.Base, Rod.Identity, MotzkinPrime);
         Rod.Nu = 1;
     }
 
@@ -70,33 +70,33 @@ abstract contract Conjecture is Feh {
     function Saturate(Fa storage Rod, uint64 _Beta, uint64 Epsilon, uint64 Theta) internal {
         if(Rod.Nu == 0) {
             Rod.Identity = _Beta;
-            Rod.Foundation = aa.modExp64(Rod.Base, Rod.Identity, MotzkinPrime);
+            Rod.Foundation = Xiao.modExp64(Rod.Base, Rod.Identity, MotzkinPrime);
         }
         assert(Rod.Nu <= 1);
         
-        uint64 Beta = aa.modExp64(Epsilon, Rod.Identity, MotzkinPrime);
-        uint64 Rho = aa.modExp64(Theta, Rod.Identity, MotzkinPrime);
-        Rod.Eta = aa.modExp64(Epsilon, Rod.Signal, MotzkinPrime);
+        uint64 Beta = Xiao.modExp64(Epsilon, Rod.Identity, MotzkinPrime);
+        uint64 Rho = Xiao.modExp64(Theta, Rod.Identity, MotzkinPrime);
+        Rod.Eta = Xiao.modExp64(Epsilon, Rod.Signal, MotzkinPrime);
 
         uint64 Phi = Rho + Rod.Eta;
         Rod.Element = Beta + Phi;
 
-        Rod.Dynamo = aa.modExp64(Theta, Rod.Signal, MotzkinPrime);
+        Rod.Dynamo = Xiao.modExp64(Theta, Rod.Signal, MotzkinPrime);
         Rod.Manifold = Rod.Element + Rod.Dynamo;
     }
 
     function Bond(Fa storage Rod) internal {
-        Rod.Dynamo = aa.modExp64(Rod.Base, Rod.Signal, Rod.Element);
+        Rod.Dynamo = Xiao.modExp64(Rod.Base, Rod.Signal, Rod.Element);
         Rod.Pole = 0;
     }
 
     function Adduct(Fa storage Rod, uint64 _Phi) internal {
-        Rod.Manifold = aa.modExp64(_Phi, Rod.Signal, Rod.Element);
+        Rod.Manifold = Xiao.modExp64(_Phi, Rod.Signal, Rod.Element);
     }
 
     function Open(Fa storage Rod) internal {
-        Rod.Ring = aa.modExp64(Rod.Coordinate, Rod.Manifold, Rod.Element);
-        Rod.Barn = aa.modExp64(Rod.Ring, Rod.Manifold, Rod.Element);
+        Rod.Ring = Xiao.modExp64(Rod.Coordinate, Rod.Manifold, Rod.Element);
+        Rod.Barn = Xiao.modExp64(Rod.Ring, Rod.Manifold, Rod.Element);
     }
 
     event DysnomiaNuclearEvent(string What, uint64 Value);
@@ -107,19 +107,19 @@ abstract contract Conjecture is Feh {
     }
 
     function Charge(Fa storage Rod, uint64 Psi) internal returns(uint64) {
-        Rod.Alpha = aa.modExp64(Rod.Barn, Psi, Rod.Ring);
+        Rod.Alpha = Xiao.modExp64(Rod.Barn, Psi, Rod.Ring);
         //emit DysnomiaNuclearEvent("Alpha Charged", Rod.Alpha);
         return Rod.Alpha;
     }
 
     function Induce(Fa storage Rod, uint64 Sigma) internal returns(uint64) {
-        Rod.Alpha = aa.modExp64(Sigma, Rod.Manifold, Rod.Ring);
+        Rod.Alpha = Xiao.modExp64(Sigma, Rod.Manifold, Rod.Ring);
         //emit DysnomiaNuclearEvent("Alpha Induced", Rod.Alpha);
         return Rod.Alpha;
     }
 
     function Torque(Fa storage Rod, uint64 Sigma) internal returns(uint64) {
-        Rod.Alpha = aa.modExp64(Sigma, Rod.Element, Rod.Channel);
+        Rod.Alpha = Xiao.modExp64(Sigma, Rod.Element, Rod.Channel);
         //emit DysnomiaNuclearEvent("Alpha TORQUE", Rod.Alpha);
         return Rod.Alpha;
     }
@@ -134,8 +134,8 @@ abstract contract Conjecture is Feh {
 
     function React(Fa storage Rod, uint64 Pi, uint64 Theta) internal {
         _mintToCap();
-        Rod.Eta = aa.modExp64(Pi, Rod.Channel, Theta);
-        Rod.Kappa = aa.modExp64(Pi, Theta, Rod.Channel);
+        Rod.Eta = Xiao.modExp64(Pi, Rod.Channel, Theta);
+        Rod.Kappa = Xiao.modExp64(Pi, Theta, Rod.Channel);
         assert(Rod.Eta != 0 && Rod.Kappa != 0);
         //emit DysnomiaNuclearEvent(">>", Rod.Eta);
         //emit DysnomiaNuclearEvent("<<", Rod.Kappa);
