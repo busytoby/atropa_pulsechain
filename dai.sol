de73ccc15f68e0cf7d7666f6c3123cdc2c7f003d
busytoby <maria@twee.se>
2024-06-10 10:27:19 -0700

    dai class for blotter

---
+++ dai.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: Sharia
+pragma solidity ^0.8.21;
+import "affinity.sol";
+
+abstract contract Dai is Affinity {
+    bytes constant private NULL = bytes(unicode"\u0009\u0009\u0009");
+
+    function Write(uint64 _v) public {
+        assert(msg.sender == owner() || Nu.Context == uint256(uint160(msg.sender)));
+        bytes memory _data = toBytes(_v);
+        Pi(_v);
+        Psi[Nu.Context][Nu.Operator].Theta = bytes.concat(Psi[Nu.Context][Nu.Operator].Theta, toBytes(_data.length), _data, NULL);
+    }
+
+    function Read() public returns(bytes memory) {
+        Chi(Mu.Upsilon);
+        return Psi[Nu.Context][Nu.Operator].Theta;
+    }
+
+    function Random() public returns(uint64) {
+        return Phi();
+    }
+}
--- rng.sol
+++ rng.sol
@@ -1,10 +1,9 @@
 // SPDX-License-Identifier: Sharia
 pragma solidity ^0.8.21;
-import "affinity.sol";
+import "dai.sol";
 
-contract Blotter is Affinity {
+contract Blotter is Dai {
     Fa private Upsilon;
-    bytes constant private NULL = bytes(unicode"\u0009\u0009\u0009");
 
     constructor() ERC20(/*name short=*/ unicode"BLOTTER™", /*symbol long=*/ unicode"🄮") Ownable(msg.sender) {
         NewDynamic(Xiao.Random(), Xiao.Random(), Xiao.Random());
@@ -14,21 +13,5 @@ contract Blotter is Affinity {
         Read();
 
         _mint(address(this), 1 * 10 ** decimals());
-    }
-
-    function Write(uint64 _v) public {
-        assert(msg.sender == owner() || Nu.Context == uint256(uint160(msg.sender)));
-        bytes memory _data = toBytes(_v);
-        Pi(_v);
-        Psi[Nu.Context][Nu.Operator].Theta = bytes.concat(Psi[Nu.Context][Nu.Operator].Theta, toBytes(_data.length), _data, NULL);
-    }
-
-    function Read() public returns(bytes memory) {
-        Chi(Mu.Upsilon);
-        return Psi[Nu.Context][Nu.Operator].Theta;
-    }
-
-    function Generate() public returns(uint64) {
-        return Phi();
     }
 }
