791b954db0410e305340a74d82e3e461042160c2
busytoby <maria@twee.se>
2024-08-01 16:33:50 -0700

    belt renamed to yi

--- 04_yi.sol
+++ 04_yi.sol
@@ -4,14 +4,14 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import "03_shio.sol";
 
-contract BELT is SH {
+contract YI is SH {
     address constant MathLib = 0x430d082e46091173B8A4f9f48752e16e3A3a4c62;
 
     SHIO internal Psi;
     uint64 internal Xi;
     uint64 internal Ring;
 
-    constructor() ERC20(unicode"VM Belt", unicode"BELT") SH(MathLib, 999) Ownable(msg.sender) {
+    constructor() ERC20(unicode"VM Yi", unicode"YI") SH(MathLib, 999) Ownable(msg.sender) {
         Psi = new SHIO(MathLib);
         Xi = Xiao.Random();
         Psi.Generate(Xi, Xiao.Random(), Xiao.Random());
