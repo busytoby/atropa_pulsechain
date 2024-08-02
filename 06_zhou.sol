9b3200112b7884d5d6d5632dbe8fa911be9cb0b7
busytoby <maria@twee.se>
2024-08-01 17:10:52 -0700

    stubbing

--- 06_zhou.sol
+++ 06_zhou.sol
@@ -8,7 +8,7 @@ import "05_zheng.sol";
 contract ZHOU is SH {
     address constant MathLib = 0x430d082e46091173B8A4f9f48752e16e3A3a4c62;
 
-    Bao internal Upsilon;
+    Fei internal Upsilon;
     ZHENG internal Theta;
 
     constructor() ERC20(unicode"VM Zhou", unicode"ZHOU") SH(MathLib, 313) Ownable(msg.sender) {
