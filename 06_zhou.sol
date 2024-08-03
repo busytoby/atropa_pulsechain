10f2de8ecb4d31c97e1393d6652cc49f47159966
busytoby <maria@twee.se>
2024-08-03 13:07:19 -0700

    stubbing

--- 06_zhou.sol
+++ 06_zhou.sol
@@ -8,7 +8,7 @@ import "05_zheng.sol";
 contract ZHOU is SH {
     string public constant Type = "ZHOU";
 
-    Fei private Upsilon;
+    uint64[] private Upsilon;
     ZHENG private Theta;
 
     constructor(address MathLib) ERC20(unicode"VM Zhou", unicode"ZHOU") SH(MathLib, 313) MultiOwnable(msg.sender) {
