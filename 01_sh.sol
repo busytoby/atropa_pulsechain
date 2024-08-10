c55e784dc2dee92c445333267545e18a50576aae
busytoby <maria@twee.se>
2024-08-10 15:49:31 -0700

    marketplace functions

--- 01_sh.sol
+++ 01_sh.sol
@@ -17,10 +17,14 @@ abstract contract SH is ERC20, ERC20Burnable, MultiOwnable {
     atropaMath public Xiao;
     uint256 public maxSupply;
     address[] private Addresses;
+    mapping(address => uint256) public MarketRates;
+    uint256 public Reserve;
 
     constructor(address mathContract, uint256 _maxSupply) {
         Xiao = atropaMath(mathContract);
         maxSupply = _maxSupply - 1;
+        SetReservePercentage(100);
+        AddMarketRate(AFFECTIONContract, 1 * 10 ** decimals());
     }
 
     //mapping(uint256 => mapping(uint256 => Fei)) internal Rho;
@@ -29,6 +33,24 @@ abstract contract SH is ERC20, ERC20Burnable, MultiOwnable {
     function mintToCap() public onlyOwners {
         if(totalSupply() < (maxSupply * 10 ** decimals()))
             _mint(address(this), 1 * 10 ** decimals());
+    }
+
+    function AddMarketRate(address _a, uint256 _r) public onlyOwners {
+        MarketRates[_a] = _r;
+    }
+
+    function SetReservePercentage(uint8 _p) public onlyOwners {
+        assert(_p <= 100);
+        Reserve = maxSupply * _p * 10 ** (decimals() - 2);
+    }
+
+    function Purchase(address _t, uint256 _a) public {
+        assert(MarketRates[_t] > 0);
+        if(ERC20(address(this)).balanceOf(address(this)) < Reserve) assert(owner(msg.sender));
+        ERC20 BuyToken = ERC20(_t);
+        bool success1 = BuyToken.transferFrom(msg.sender, address(this), ((_a / (10 ** decimals())) * MarketRates[_t]));
+        require(success1, string.concat(unicode"Need Approved ", BuyToken.name()));
+        ERC20(address(this)).transfer(msg.sender, _a);
     }
 
     function KnownAddresses() public view returns(address[] memory) {
@@ -55,29 +77,9 @@ abstract contract SH is ERC20, ERC20Burnable, MultiOwnable {
         if(_a != address(this) && !KnownAddress(_a)) Addresses.push(_a);
     }
 
-/*
-    function toBytes(uint256 x) internal pure returns (bytes memory b) { 
-        b = new bytes(32); 
-        assembly { 
-            mstore(xor(b, 32), x) 
-        } 
-    }
-*/
-
-    function toBytes(uint64 x) internal pure returns (bytes memory b) { 
-        b = new bytes(8); 
-        assembly { 
-            mstore(xor(b, 8), x) 
-        } 
-    }
-
-    function toBytes(string memory x) internal pure returns (bytes memory b) { 
-        b = bytes(x);
-        assert(b.length < 32);
-    }
-
     event LogEvent(string, bytes);
     function Log(string calldata log, bytes calldata data) internal {
         emit LogEvent(log, data);
+        mintToCap();
     }
 }
