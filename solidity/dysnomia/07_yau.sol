bb3713e80bc19e69b6f4899e81ad00ce97af17c4
busytoby <maria@twee.se>
2024-08-20 13:38:36 -0700

    etc

--- solidity/dysnomia/07_yau.sol
+++ solidity/dysnomia/07_yau.sol
@@ -49,6 +49,7 @@ contract YAU is DYSNOMIA {
         uint64 _e = Theta.Mu.Flux(SHA(SHIO(Theta.Shio).Cone()).View().Monopole);
         uint64 _m = Theta.Mu.Flux(Theta.Omega);
         Bao memory Delta = Tau.Upsilon().Eta().React(Theta, _m);
+        Theta.Omicron = SHA(SHIO(Theta.Shio).Cone()).View().Monopole ^ Delta.Omega;
         uint64 Dysjecta = Xiao.modExp64(_b, _e, _m);
         
         mintToCap();
