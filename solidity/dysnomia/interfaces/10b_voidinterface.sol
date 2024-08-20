5e8ed6da25a8def17a2eecb6951cd15c73a58137
busytoby <maria@twee.se>
2024-08-20 12:24:14 -0700

    void interface functions

--- solidity/dysnomia/interfaces/10b_voidinterface.sol
+++ solidity/dysnomia/interfaces/10b_voidinterface.sol
@@ -9,12 +9,18 @@ interface VOID {
     function Type() external returns(string memory);
     function Nu() external returns(SIU);
     function AddLibrary(string memory name, address _a) external;
+    function Clear(uint64 Iota) external returns(uint64);
+    function Sign(uint64 DIRAC1) external returns(Bao memory);
     function Log(string memory LogLine) external;
     function Log(uint64 Sigma, string memory LogLine) external;
     function Log(address Sigma, string memory LogLine) external;
     function Log(string memory Xi, string memory LogLine) external;
     function SetAttribute(string memory name, string memory value) external;
     function GetAttribute(string memory name) external view returns (string memory);
+    function Alias(address name, string memory value) external;
+    function Alias(address name) external view returns (string memory);
+    function Alias(Bao memory Theta, string memory value) external;
+    function Alias(Bao memory Theta) external view returns (string memory);
     function Enter() external returns(uint64[3] memory Saat, Bao memory On);
     function Enter(string memory name, string memory symbol) external returns(uint64[3] memory Saat, Bao memory On);
 }
