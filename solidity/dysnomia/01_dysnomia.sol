e8560c3021b29fff2625c85d9cc5270e0286ef63
busytoby <maria@twee.se>
2024-08-22 16:04:51 -0700

    simpler

--- solidity/dysnomia/01_dysnomia.sol
+++ solidity/dysnomia/01_dysnomia.sol
@@ -189,41 +189,21 @@ abstract contract DYSNOMIA is MultiOwnable {
 
     function log10(uint256 value) internal pure returns (uint256) {
         uint256 result = 0;
+
         unchecked {
-            if (value >= 10 ** 64) {
-                value /= 10 ** 64;
-                result += 64;
-            }
-            if (value >= 10 ** 32) {
-                value /= 10 ** 32;
-                result += 32;
-            }
-            if (value >= 10 ** 16) {
-                value /= 10 ** 16;
-                result += 16;
-            }
-            if (value >= 10 ** 8) {
-                value /= 10 ** 8;
-                result += 8;
-            }
-            if (value >= 10 ** 4) {
-                value /= 10 ** 4;
-                result += 4;
-            }
-            if (value >= 10 ** 2) {
-                value /= 10 ** 2;
-                result += 2;
-            }
-            if (value >= 10 ** 1) {
-                result += 1;
+            for(uint8 _e = 64; _e >= 1; _e /= 2) {
+                if (value >= 10 ** _e) {
+                    value /= 10 ** _e;
+                    result += _e;
+                }
             }
         }
         return result;
     }
 
-    function String(uint64 value) internal pure returns (string memory) {
-        return String(uint256(value));
-    }
+    //function String(uint64 value) internal pure returns (string memory) {
+    //    return String(uint256(value));
+    //}
 
     function String(uint256 value) internal pure returns (string memory) {
         unchecked {
