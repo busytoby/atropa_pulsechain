7d2ade95035a5449659638fa26cab1e450517b0f
busytoby <maria@twee.se>
2024-09-07 15:44:46 -0700

    restrict to exactly & only 1 qing per integrative token

--- solidity/dysnomia/13c_qingfactory.sol
+++ solidity/dysnomia/13c_qingfactory.sol
@@ -5,17 +5,21 @@ import "./interfaces/libyai.sol";
 
 contract QINGFactory {
     LIBYAI public Yai;
+    mapping(address => address) private _qings;
 
     constructor(address YAIAddress) {
         Yai = LIBYAI(YAIAddress);
         Yai.addOwner(address(this));
     } 
 
+    error QingAlreadyExists(address Token, address Qing);
     error ForbiddenToken(address which);
     function New(address Integrative) public returns(QING Mu) {
+        if(_qings[Integrative] != address(0x0)) revert QingAlreadyExists(Integrative, _qings[Integrative]);
         if(Yai.IsForbidden(Integrative)) revert ForbiddenToken(Integrative);
         Mu = new QING(Integrative, address(Yai.Void()));
         Yai.addOwner(address(Mu));
         Mu.renounceOwnership(address(this));
+        _qings[Integrative] = address(Mu);
     }
 }
