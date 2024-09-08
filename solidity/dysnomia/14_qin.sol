a750139970eec2f1440d9e2d88a3a1ad253736f7
busytoby <maria@twee.se>
2024-09-08 11:39:59 -0700

    stubbing

---
+++ solidity/dysnomia/14_qin.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: Sharia
+pragma solidity ^0.8.21;
+import "./include/user.sol";
+import "./01_dysnomia_v2.sol";
+import "./interfaces/13b_qinginterface.sol";
+import "./interfaces/13d_qingfactoryinterface.sol";
+
+contract QIN is DYSNOMIA {
+    VOID public Void;
+    uint64[3] public Saat;
+    Bao public On;
+
+    constructor(string memory name, string memory symbol, address VoidAddress) DYSNOMIA(name, symbol, address(DYSNOMIA(VoidAddress).Xiao())) {
+    }
+}
---
+++ solidity/dysnomia/interfaces/13d_qingfactoryinterface.sol
@@ -0,0 +1,8 @@
+    // SPDX-License-Identifier: Sharia
+pragma solidity ^0.8.21;
+import "./13b_qinginterface.sol";
+
+interface QINGFactory {  
+    function Yai() external returns(LIBYAI);
+    function New(address Integrative) external returns(QING Mu);
+}
