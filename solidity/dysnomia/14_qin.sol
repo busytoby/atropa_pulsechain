0c91b9d128993fb6c1558644ae15c69eeb60bfd4
busytoby <maria@twee.se>
2024-09-08 12:41:13 -0700

    stubbing

--- solidity/dysnomia/14_qin.sol
+++ solidity/dysnomia/14_qin.sol
@@ -6,9 +6,9 @@ import "./interfaces/13b_qinginterface.sol";
 import "./interfaces/13d_qingfactoryinterface.sol";
 
 contract QIN is DYSNOMIA {
-    VOID public Void;
     uint64[3] public Saat;
     Bao public On;
+    address[3][] private _inventory;
 
     constructor(string memory name, string memory symbol, address VoidQingAddress) DYSNOMIA(name, symbol, address(DYSNOMIA(VoidQingAddress).Xiao())) {
         addOwner(tx.origin);
@@ -16,5 +16,37 @@ contract QIN is DYSNOMIA {
 
     function AddMarketRate(address _a, uint256 _r) public onlyOwners {
         _addMarketRate(_a, _r);
+    }
+
+    function GetInventoryCount(uint256 class) public view returns (uint256) {
+        return _inventory[class].length;
+    }
+
+    function GetDefense(uint256 n) public view returns (address) {
+        return _inventory[0][n];
+    }
+
+    function GetDecoration(uint256 n) public view returns (address) {
+        return _inventory[1][n];
+    }
+
+    function GetOffense(uint256 n) public view returns (address) {
+        return _inventory[2][n];
+    }
+
+    function Throw() public view returns (uint64) {
+        // TODO
+    }
+
+    function Save(uint64 pQi) public view returns (uint64) {
+        // TODO
+    }
+
+    function Equip(address qi) public onlyOwners {
+        // TODO
+    }
+
+    function Remove(address qi) public onlyOwners {
+        // TODO
     }
 }
