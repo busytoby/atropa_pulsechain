5e66d48c830f287702debc7bcf18c079bf7709ec
busytoby <maria@twee.se>
2024-09-08 12:50:16 -0700

    no Saat on QI

--- solidity/dysnomia/14_qi.sol
+++ solidity/dysnomia/14_qi.sol
@@ -6,11 +6,12 @@ import "./interfaces/13b_qinginterface.sol";
 import "./interfaces/13d_qingfactoryinterface.sol";
 
 contract QI is DYSNOMIA {
-    uint64[3] public Saat;
     Bao public On;
     uint256 public q;
 
-    constructor(string memory name, string memory symbol, address VoidQingAddress) DYSNOMIA(name, symbol, address(DYSNOMIA(VoidQingAddress).Xiao())) {
+    constructor(bytes memory Geng, address VoidQingAddress) DYSNOMIA("Qi", "QI", address(DYSNOMIA(VoidQingAddress).Xiao())) {
         addOwner(tx.origin);
+
+
     }
 }
