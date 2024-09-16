9324be35a2f5afe80c3b85f892efe2ea8ff9fbb2
busytoby <maria@twee.se>
2024-09-15 17:05:11 -0700

    nope

--- solidity/dysnomia/14_qi.sol
+++ solidity/dysnomia/14_qi.sol
@@ -27,22 +27,14 @@ contract QI is DYSNOMIA {
     }
 
     function GetUserDepositsIds(uint64 UserSoul) public view returns (uint256[] memory DepositIds) {
-        if(!owner(msg.sender)) {
-            uint64 _soul = Cho.GetUserSoul();
-            if(UserSoul != _soul) revert NotOwner(UserSoul);
-        }
         return _userDepositIndexes[UserSoul];
     }
 
-    function GetQingDepositIds(uint256 QingWaat) public view onlyOwners returns (uint256[] memory DepositIds) {
+    function GetQingDepositIds(uint256 QingWaat) public view returns (uint256[] memory DepositIds) {
         return _qingDepositIndexes[QingWaat];
     }
 
     function GetDeposit(uint256 Id) public view returns (TimeLockedQiDeposit memory Stake) {
-        if(!owner(msg.sender)) {
-            uint64 _soul = Cho.GetUserSoul();
-            if(_deposits[Id].soul != _soul) revert NotOwner(Id);
-        }
         return _deposits[Id];
     }
 
