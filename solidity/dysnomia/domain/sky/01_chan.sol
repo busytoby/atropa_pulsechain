613851230d60517d07132578d7e16fd593cace36
busytoby <busytoby@gmail.com>
2024-09-21 10:38:48 -0700

    more clear error

--- solidity/dysnomia/domain/sky/01_chan.sol
+++ solidity/dysnomia/domain/sky/01_chan.sol
@@ -8,19 +8,19 @@ contract CHAN is DYSNOMIA {
     string public constant Type = "CHAN";
 
     XIE public Xie;
-    
+
     constructor(address XieAddress) DYSNOMIA("Dysnomia Chan", "CHAN", address(DYSNOMIA(XieAddress).Xiao())) {
         Xie = XIE(XieAddress);
         addOwner(tx.origin);
         _mintToCap();
     }
 
-    error InvalidQinOwnership(uint256 Maat, uint256 QinMaat);
+    error InvalidQinInstance(uint64 Soul, uint256 SoulMaat, uint256 QinMaat);
     function React(QIN memory Player, string memory Adjective) public returns (uint256 Charge, uint64 Omicron, uint64 Omega) {
+        uint64 Soul = Xie.Xia().Mai().Qi().Zuo().Cho().GetUserSoul();
         uint256 Maat = Xie.Xia().Mai().Maat();
-        if(Maat != Player.Maat) revert InvalidQinOwnership(Maat, Player.Maat);
-
-        uint64 _soul = Xie.Xia().Mai().Qi().Zuo().Cho().GetUserSoul();
-        (Charge, Omicron, Omega) = Xie.React(_soul, Adjective);
+        if(Maat != Player.Maat) revert InvalidQinInstance(Soul, Maat, Player.Maat);
+        (Charge, Omicron, Omega) = Xie.React(Soul, Adjective);
+        _mintToCap();
     }
 }
