733ec89247f03210366eebab006df29f4bf4ecd1
busytoby <busytoby@gmail.com>
2024-09-26 16:57:14 -0700

    wtlie

--- solidity/dysnomia/16_xia.sol
+++ solidity/dysnomia/16_xia.sol
@@ -7,10 +7,7 @@ contract XIA is DYSNOMIA {
     string public constant Type = "XIA";
 
     MAI public Mai;
-    uint256 private _depositAmount = 10000000000000000000000;
-    uint256 public r = 90745943887872000;
-    uint256 public s = 10736731045232640;
-    uint256 public Monster = 4154781481226426191177580544000000;
+    uint256 public e = 75140154104069000000000;
     uint256 public Balance;
     TimeDeposit[] private _deposits;
     mapping(uint64 UserSoul => uint256[] DepositIds) private _userDepositIndexes;
@@ -29,13 +26,13 @@ contract XIA is DYSNOMIA {
         return Xiao.modExp(_b, _e, _m);
     }
 
-    function Decay(uint256 Power) public view returns (uint256) {        
-        while(Power > Monster) Power = Power / 2;
+    function Decay(uint256 Power) public view returns (uint256) {
+        if(Power > 2 * 10 ** 25) return Power / 2;
         return Power - ((Power * 10 ** 20) / Lambda());
     }
 
     function Lambda() public view returns (uint256) {
-        return (2 * 10 ** 38) / ((s * r) / Balance);
+        return (2 * 10 ** 38) / (10000000001 - (Balance / e));
     }
 
     function GetUserDepositCount(uint64 UserSoul) public view returns (uint256) {
@@ -62,10 +59,10 @@ contract XIA is DYSNOMIA {
     error WaatMismatch(address Qing, uint256 Waat);
     error UnknownQing(address Qing);
     error ExceedsMaxSystemDeposit(uint256 MaxDepositRemaining, uint256 RequestedDeposit);
-    function Deposit(address Qing, string memory Adjective) public {
+    function Deposit(address Qing, string memory Adjective, uint256 amount) public {
         TimeDeposit memory _t;
         
-        if(Balance + _depositAmount > r * s) revert ExceedsMaxSystemDeposit((r * s) - Balance, _depositAmount);
+        if(Balance + amount > e * 10000000000) revert ExceedsMaxSystemDeposit((e * 10000000000) - Balance, amount);
 
         _t.waat = QING(Qing).Waat();
         if(_t.waat == 0) revert UnknownQing(Qing);
@@ -76,36 +73,36 @@ contract XIA is DYSNOMIA {
         uint64 _soul = Mai.Qi().Zuo().Cho().GetUserSoul();
         address Freebies = Mai.Qi().Zuo().Cho().Addresses("Freebies");
         DYSNOMIA withdrawToken = DYSNOMIA(Freebies);
-        withdrawToken.transferFrom(msg.sender, address(this), _depositAmount);
+        withdrawToken.transferFrom(msg.sender, address(this), amount);
 
         _t.soul = _soul;
         if(_userQingAdjectiveDeposits[_soul][_t.waat][Adjective] == 0)
             _t.depositId = _deposits.length;
         else 
             _t.depositId = _userQingAdjectiveDeposits[_soul][_t.waat][Adjective];
-        _t.amount += _depositAmount;
+        _t.amount += amount;
         _t.timestamp = block.timestamp;
         _deposits.push(_t);
         _userDepositIndexes[_soul].push(_t.depositId);
         _userQingAdjectiveDeposits[_soul][_t.waat][Adjective] = _t.depositId;
 
-        Balance += _depositAmount;
+        Balance += amount;
         _mintToCap();
     }
 
     error NotOwner(uint256 DepositId);
     error ExceedsBalance(uint256 DepositId, uint256 Balance);
-    function Withdraw(uint256 Id) public {
+    function Withdraw(uint256 Id, uint256 Amount) public {
         uint64 _soul = Mai.Qi().Zuo().Cho().GetUserSoul();
         if(_deposits[Id].soul != _soul) revert NotOwner(Id);
-        if(_depositAmount > _deposits[Id].amount) revert ExceedsBalance(Id, _deposits[Id].amount);
+        if(Amount > _deposits[Id].amount) revert ExceedsBalance(Id, _deposits[Id].amount);
 
         address Freebies = Mai.Qi().Zuo().Cho().Addresses("Freebies");
         DYSNOMIA withdrawToken = DYSNOMIA(Freebies);
-        withdrawToken.transfer(msg.sender, _depositAmount);
-        _deposits[Id].amount -= _depositAmount;
+        withdrawToken.transfer(msg.sender, Amount);
+        _deposits[Id].amount -= Amount;
 
-        Balance -= _depositAmount;
+        Balance -= Amount;
         _mintToCap();
     }
 }
