be63b85afacdcdbed6bdaa2e1b87300d20da7e92
busytoby <busytoby@gmail.com>
2024-09-29 10:40:15 -0700

    stubbing

---
+++ solidity/dysnomia/domain/operations/01_sei.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: Sharia
+pragma solidity ^0.8.21;
+import "../../01_dysnomia_v2.sol";
+import "../w00t.sol";
+
+contract SEI is DYSNOMIA {
+    string public constant Type = "SEI";
+
+    CHAN public Chan;
+
+    constructor(address ChanAddress) DYSNOMIA("Dysnomia Sei", "SEI", address(DYSNOMIA(ChanAddress).Xiao())) {
+        Chan = CHAN(ChanAddress);
+        //Chan.Xie().Xia().Mai().Qi().Zuo().VAT().addOwner(address(this));
+        addOwner(tx.origin);
+        //addOwner(address(Chan.Xie().Xia().Mai().Qi().Zuo().VAT()));
+        _mintToCap();
+    }
+}
---
+++ solidity/dysnomia/domain/sky/interfaces/02b_choainterface.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: Sharia
+pragma solidity ^0.8.21;
+import "./01b_chaninterface.sol";
+
+interface CHOA {
+    function maxSupply() external view returns(uint256);
+    function Rename(string memory newName, string memory newSymbol) external;
+    function GetMarketRate(address _a) external view returns(uint256);
+    function Purchase(address _t, uint256 _a) external;
+    function Redeem(address _t, uint256 _a) external;
+    function name() external view returns (string memory);
+    function symbol() external view returns (string memory);
+    function decimals() external view returns (uint8);
+    function totalSupply() external view returns (uint256);
+    function balanceOf(address account) external view returns (uint256);
+    function transfer(address to, uint256 value) external returns (bool);
+    function allowance(address owner, address spender) external view returns (uint256);
+    function approve(address spender, uint256 value) external returns (bool);
+    function transferFrom(address from, address to, uint256 value) external returns (bool);
+    function addOwner(address newOwner) external;
+    function renounceOwnership(address toRemove) external;
+    function owner(address cOwner) external view returns (bool);
+    function mintToCap() external;
+    function Type() external view returns (string memory);
+    function Chan() external view returns (CHAN);
+}
