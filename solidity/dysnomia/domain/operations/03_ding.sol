575effd63f699ad1fa6fccd59e9277ab253e7faa
busytoby <busytoby@gmail.com>
2024-10-04 19:00:48 -0700

    cleanup

--- solidity/dysnomia/domain/operations/03_ding.sol
+++ solidity/dysnomia/domain/operations/03_ding.sol
@@ -16,11 +16,15 @@ contract DING is DYSNOMIA {
     }
 
     function Train(TRAIT Trait) public returns (uint256 Charge, uint256 UserQi, uint256 Omicron, uint256 Omega) {
+        (YUEINTERFACE Yue, , QIN memory Player) = Cheon.Sei().Chi();
+        return _react(Yue, Player, Trait);
+    }
+
+    function _react(YUEINTERFACE Yue, QIN memory Player, TRAIT Trait) internal returns (uint256 Charge, uint256 UserQi, uint256 Omicron, uint256 Omega) {
         uint256 Hypogram;
         uint256 Epigram;
         uint256 Contour;
         TimeDeposit memory _t;
-        (YUEINTERFACE Yue, , QIN memory Player) = Cheon.Sei().Chi();
         (Charge, UserQi, Omega) = Cheon.Sei().Chan().React(Player, Trait);
         Omicron = Yue.React(Trait);
         (Hypogram, Epigram) = Yue.Bar(Trait);
@@ -31,19 +35,7 @@ contract DING is DYSNOMIA {
     }
 
     function React(uint64 Soul, TRAIT Trait) public onlyOwners returns (uint256 Charge, uint256 UserQi, uint256 Omicron, uint256 Omega) {
-        uint256 Hypogram;
-        uint256 Epigram;
-        uint256 Contour;
-        TimeDeposit memory _t;
-        YUEINTERFACE Yue = YUEINTERFACE(Cheon.Sei().Chan().Yan(Cheon.Sei().Chan().Xie().Xia().Mai().Qi().Zuo().Cho().GetAddressBySoul(Soul)));
-        QIN memory Player =  Cheon.Sei().Chan().Xie().Xia().Mai().GetPlayerQin(Soul);
-
-        (Charge, UserQi, Omega) = Cheon.Sei().Chan().React(Player, Trait);
-        Omicron = Yue.React(Trait);
-        (Hypogram, Epigram) = Yue.Bar(Trait);
-        _t = Cheon.Sei().Chan().Xie().Xia().Mai().GetQingDeposit(Player.Location.Waat());
-        Contour = Cheon.Sei().Chan().Xie().Xia().Amplify(_t.amount, _t.timestamp);
-        Omega = Xiao.modExp(Hypogram, Omega, Contour);
-        Omicron = Xiao.modExp(Epigram, Omicron, Contour);
+        (YUEINTERFACE Yue, , QIN memory Player) = Cheon.Sei().ChiBySoul(Soul);
+        return _react(Yue, Player, Trait);
     }
 }
