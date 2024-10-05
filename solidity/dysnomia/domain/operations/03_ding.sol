696b9102b6447a5671bd0e8338514686215ff6e7
busytoby <busytoby@gmail.com>
2024-10-04 18:21:18 -0700

    maybe dysone contour

--- solidity/dysnomia/domain/operations/03_ding.sol
+++ solidity/dysnomia/domain/operations/03_ding.sol
@@ -18,13 +18,15 @@ contract DING is DYSNOMIA {
     function React(TRAIT Trait) public onlyOwners returns (uint256 Charge, uint256 UserQi, uint256 Omicron, uint256 Omega) {
         uint256 Hypogram;
         uint256 Epigram;
-        uint256 QingDeposit;
+        uint256 Contour;
+        TimeDeposit memory _t;
         (YUEINTERFACE Yue, , QIN memory Player) = Cheon.Sei().Chi();
         (Charge, UserQi, Omega) = Cheon.Sei().Chan().React(Player, Trait);
         Omicron = Yue.React(Trait);
         (Hypogram, Epigram) = Yue.Bar(Trait);
-        QingDeposit = Cheon.Sei().Chan().Xie().Xia().Mai().GetQingDeposit(Player.Location.Waat()).amount;
-        Omega = Xiao.modExp(Hypogram, Omega, QingDeposit);
-        Omicron = Xiao.modExp(Epigram, Omicron, QingDeposit);
+        _t = Cheon.Sei().Chan().Xie().Xia().Mai().GetQingDeposit(Player.Location.Waat());
+        Contour = Cheon.Sei().Chan().Xie().Xia().Amplify(_t.amount, _t.timestamp);
+        Omega = Xiao.modExp(Hypogram, Omega, Contour);
+        Omicron = Xiao.modExp(Epigram, Omicron, Contour);
     }
 }
