ea7596ce6539dd3c19e343f7f966dd16cfed644d
busytoby <busytoby@gmail.com>
2024-10-09 09:32:26 -0700

    deprecated

--- solidity/dysnomia/include/qin.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: Sharia
-pragma solidity ^0.8.21;
-import "../interfaces/13b_qinginterface.sol";
-
-struct QIN {
-    uint256 Maat;
-    QINGINTERFACE Location;
-    uint256 lastMove;
-}
