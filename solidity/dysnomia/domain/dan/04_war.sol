2bbfb8faa4c6cca4915f34aa69f39a4d120c13b2
busytoby <busytoby@gmail.com>
2024-10-27 19:53:50 -0700

    stubbing

--- solidity/dysnomia/domain/dan/04_war.sol
+++ solidity/dysnomia/domain/dan/04_war.sol
@@ -1,14 +1,20 @@
 // SPDX-License-Identifier: Sharia
 pragma solidity ^0.8.21;
 import "../../01_dysnomia_v2.sol";
+import "../interfaces/worldinterface.sol";
 
 contract WAR is DYSNOMIA {
     string public constant Type = "WAR";
 
+    WORLDINTERFACE public World;
+
     constructor(address WorldAddress) DYSNOMIA("Dysnomia War", "WAR", address(DYSNOMIA(WorldAddress).Xiao())) {
+        World = WORLDINTERFACE(WorldAddress);
+
         uint256 originMint = Xiao.Random() % maxSupply / 10;
         _mint(tx.origin, originMint * 10 ** decimals());
 
         _addMarketRate(AFFECTIONContract, 1 * 10 ** decimals());
+        _addMarketRate(WorldAddress, 1 * 10 ** decimals());
     }
 }
