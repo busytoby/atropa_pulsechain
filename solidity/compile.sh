94cb2d69d2d59c60e0340a61c7fa17965c89eeb5
maria <maria@twee.se>
2024-10-30 17:37:31 -0700

    generate json files equivalent to what is used in app

--- solidity/compile.sh
+++ solidity/compile.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-for i in `find dysnomia -newer last_compile |grep -i sol$ |grep -v interfaces`; do ../../../solc.exe --bin --abi --optimize --optimize-runs=200 -o C:\\Users\\mariarahel\\source\\repos\\atropa_pulsechain\\Wallet\\bin\\Contracts --evm-version=shanghai $i --overwrite; done; touch last_compile
+for i in `find dysnomia -newer last_compile |grep -i sol$ |grep -v interfaces`; do ../../../solc.exe --combined-json=bin,abi --optimize --optimize-runs=200 --evm-version=shanghai $i > ../Wallet/bin/Contracts/`basename $i`.json; done; touch last_compile
