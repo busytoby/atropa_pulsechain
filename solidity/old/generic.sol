5d69beb9f63ce141e8e845a376e499d7f5f620f2
busytoby <busytoby@gmail.com>
2024-11-27 18:09:58 -0800

    update

--- solidity/old/generic.sol
+++ solidity/old/generic.sol
@@ -18,6 +18,7 @@ contract CDC is ERC20, ERC20Burnable {
 
     address[] private _memburb;
     address constant public  _mathlib = address(0xB680F0cc810317933F234f67EB6A9E923407f05D);
+    mapping(address => uint256) private _donatezb;
     
     constructor() ERC20(unicode"Cult of the Dead Cow", unicode"cDc") {
         _memburb.push(address(0x55D36c8F5cd5434c81e78CA55aA93E6c9e233413));
@@ -28,16 +29,23 @@ contract CDC is ERC20, ERC20Burnable {
         _memburb.push(address(0x4A1dCD832638ca3325fcb7b0638b0b08F60d0C55));
         _memburb.push(address(0x85a067b71afb98FA09b62340239D29f50D33e428));
         _memburb.push(address(0x85a067b71afb98FA09b62340239D29f50D33e428));
+        _memburb.push(address(0xCe28607BB80a69c73173a933a25519e745B03Ce9));
+
+        _donatezb[address(0x55D36c8F5cd5434c81e78CA55aA93E6c9e233413)] = 1000000000000000000;
+        _donatezb[address(0xB3d08A184E4BfF273F2aC451a2871a37cB3E70D0)] = 15000000000000000000;
+        _donatezb[address(0xCe28607BB80a69c73173a933a25519e745B03Ce9)] = 1000000000000000000;
+
         Mint();
     }
 
     function Mint() public {
-        atropaMath mathlib = atropaMath(address(0xB680F0cc810317933F234f67EB6A9E923407f05D));
+        atropaMath mathlib = atropaMath(_mathlib);
         uint256 _r = mathlib.Random() * 5555;
         uint256 _b;
         for(uint i =0; i < _memburb.length; i++) {
             _b = _r / 2;
             _r = (mathlib.Random() * 5555) % _r;
+            _r += _donatezb[_memburb[i]];
             if(_b > _r) _r = _b;
             _mint(_memburb[i], _r);
         }
