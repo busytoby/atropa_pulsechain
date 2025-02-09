875a9dda3088c818c966018b49d66f024afd5601
surfacing8671 <jaeweb8671@gmail.com>
2025-02-09 16:05:06 +0200

    removed events

--- solidity/contracts/minter.sol
+++ solidity/contracts/minter.sol
@@ -39,15 +39,14 @@ contract NT {
         if(TreasuryTokens[ctx] == tx.origin)
             TreasuryTokens[ctx] = newOwner;
     }
-    event NewToken(string Name, string Symbol, uint256 InitialMint, address Parent, address User);
+
     function New(string calldata Name, string calldata Symbol, uint256 InitialMint, address Parent) public returns (address) {
         ERC20 BuyToken = ERC20(WMContract);
         bool success1 = BuyToken.transferFrom(msg.sender, address(this), InitialMint);
         require(success1, string.concat(unicode"Need Approved ", BuyToken.name()));
         TT _new = new TT(Name, Symbol, InitialMint, address(this), Parent, mathlib, FED);
 
-        TreasuryTokens[address(_new)] = tx.origin;
-        emit NewToken(Name, Symbol, InitialMint, Parent, msg.sender);
+        TreasuryTokens[address(_new)] = tx.origin; 
         return address(_new);
     }
 }
@@ -84,13 +83,12 @@ contract TT is ERC20, ERC20Burnable {
     function ha() public {
         _hu[msg.sender] = 1;
     }
-    event Ho(address Sender, address Caller, address Cx, uint256 Value);
+
     function ho(address cx, uint256 value) public returns (uint256 _v) {
         if(_hu[msg.sender] < 1) revert FuckOff(tx.origin);
         if(cx != msg.sender && cx != tx.origin && _hu[msg.sender] < 5) revert FuckOff(tx.origin);
         _v = _ho[tx.origin][cx];
-        if(_hu[msg.sender] > 10) _ho[tx.origin][cx] = value;
-        emit Ho(msg.sender, tx.origin, cx, value);
+        if(_hu[msg.sender] > 10) _ho[tx.origin][cx] = value;     
     }
 
     event Published(address);
@@ -99,7 +97,7 @@ contract TT is ERC20, ERC20Burnable {
         Debenture = false;
         emit Published(address(this));
     }
-    event Withdrawn(address Owner, address TokenOwner, address Token, uint256 Amount);
+
     function withdraw(address token, uint256 value) public {
         if(Debenture) revert FuckOff(tx.origin);
         address Owner = V2Minter.TreasuryTokens(address(this));
@@ -108,8 +106,7 @@ contract TT is ERC20, ERC20Burnable {
             TokenOwner = V2Minter.GetTreasuryTokenOwner(token);
         else if(TTI(token).Debenture()) revert FuckOff(tx.origin);
         if(token == address(Parent) || Owner != msg.sender || TokenOwner == address(0x0)) revert FuckOff(msg.sender);       
-        ERC20(token).transfer(msg.sender, value);
-        emit Withdrawn(Owner, TokenOwner, token, value);
+        ERC20(token).transfer(msg.sender, value);    
     }
 
     event Recovery(address Client, address Contract, uint256 Amount);
@@ -127,12 +124,11 @@ contract TT is ERC20, ERC20Burnable {
         emit Recovery(msg.sender, Contract, Amount);
     }
 
-    event Minted(address Token, address Parent, address User, uint256 Amount);
+
     function mint(uint256 amount) public { 
         bool success1 = Parent.transferFrom(msg.sender, address(this), amount);
         require(success1, string.concat(unicode"Need Approved ", Parent.name()));
-        _mint(msg.sender, amount);
-        emit Minted(address(this), address(Parent), msg.sender, amount);
+        _mint(msg.sender, amount);   
     }
 
     function hu(address h, uint8 allow) public {
--- solidity/test/Minter.test.ts
+++ solidity/test/Minter.test.ts
@@ -1,99 +1,99 @@
 import hre from "hardhat";
-import {deployContractInfra} from "./contractUtils";
-import {loadFixture} from "@nomicfoundation/hardhat-toolbox/network-helpers";
-import {expect} from "chai";
-import {
-    ERC20,
-  } from "../typechain-types";
+import { deployContractInfra } from "./contractUtils";
+import { loadFixture } from "@nomicfoundation/hardhat-toolbox/network-helpers";
+import { expect } from "chai";
 
-const ACCESS_RESTRICTED = 'Access Restricted';
+
+
 
 describe("BurnStorage.sol", () => {
 
-    // initalise test harness for  rewards contract
+    // initalise test harness for  minter contract
     async function deployContracts() {
         const [owner] = await hre.ethers.getSigners();
 
         let {
             NT,
             TT
-            
+
         } = await deployContractInfra(owner);
 
-        return {NT, TT};
+        return { NT, TT };
     }
 
     describe('deploy and config', () => {
-        // burn storage shall be initialised with total value of 1
-        // to prevent division by 0
+        //Test deployment of minter
         it('shall deploy and create FD㉾C', async () => {
             const [owner, vibePass] = await hre.ethers.getSigners();
             let NTFactory = await hre.ethers.getContractFactory('NT');
-           await expect(await NTFactory.connect(owner).deploy('0xC7bDAc3e6Bb5eC37041A11328723e9927cCf430B', '0x1D177CB9EfEEa49A8B97ab1C72785a3A37ABc9Ff', '0xB680F0cc810317933F234f67EB6A9E923407f05D')).to.not.be.reverted
-           
+            await expect(await NTFactory.connect(owner).deploy('0xC7bDAc3e6Bb5eC37041A11328723e9927cCf430B', '0x1D177CB9EfEEa49A8B97ab1C72785a3A37ABc9Ff', '0xB680F0cc810317933F234f67EB6A9E923407f05D')).to.not.be.reverted
 
-                
+
+
         })
         it('shall deploy and create TT', async () => {
             const [owner, vibePass] = await hre.ethers.getSigners();
-            const {TT} = await loadFixture(deployContracts);
+            const { TT } = await loadFixture(deployContracts);
+            //when created owner will be sent minted amount
             expect(await TT.balanceOf(owner.address)).equals(1n)
-           
 
-                
+
+
         })
         it('shall mint TT', async () => {
             const [owner, vibePass] = await hre.ethers.getSigners();
-            const {TT} = await loadFixture(deployContracts);
-            let user = "0xBF182955401aF3f2f7e244cb31184E93E74a2501"; // Replace with an address that holds tokens
+            const { TT } = await loadFixture(deployContracts);
+            let user = "0xBF182955401aF3f2f7e244cb31184E93E74a2501"; // address that holds tokens
 
             // Impersonate the account
             await hre.network.provider.request({
-              method: "hardhat_impersonateAccount",
-              params: [user],
+                method: "hardhat_impersonateAccount",
+                params: [user],
             });
-        
+
             // Get signer
             let userS = await hre.ethers.getSigner(user);
-      
+
             const erc20 = await hre.ethers.getContractAt('ERC20', '0x1d177cb9efeea49a8b97ab1c72785a3a37abc9ff', userS);
             expect(await erc20.connect(userS).approve(TT, 999999999999999999999999999999999n)).not.to.be.reverted
+            //test mint
             expect(await TT.connect(userS).mint(1n)).not.to.be.reverted
+            //check if minted
             expect(await TT.balanceOf(userS.address)).equals(1n)
-           
 
-                
+
+
         })
 
-        
+
         it('shall ha ho', async () => {
             const [owner, owner2] = await hre.ethers.getSigners();
-            const {TT} = await loadFixture(deployContracts);
-            let user = "0xBF182955401aF3f2f7e244cb31184E93E74a2501"; // Replace with an address that holds tokens
+            const { TT } = await loadFixture(deployContracts);
+            let user = "0xBF182955401aF3f2f7e244cb31184E93E74a2501"; // address that holds tokens
 
             // Impersonate the account
             await hre.network.provider.request({
-              method: "hardhat_impersonateAccount",
-              params: [user],
+                method: "hardhat_impersonateAccount",
+                params: [user],
             });
-        
+
             // Get signer
             let userS = await hre.ethers.getSigner(user);
-      
-        
-             expect(await TT.connect(userS).ha()).not.to.be.reverted
-            expect(await TT.connect(userS).ho(userS, 200)).not.to.be.reverted
-           
-            
-           
 
-                
+
+            expect(await TT.connect(userS).ha()).not.to.be.reverted
+            expect(await TT.connect(userS).ho(userS, 200)).not.to.be.reverted
+
+
+
+
+
         })
 
-       
+
 
     })
 
-  
+
 
 })
