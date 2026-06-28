b925eb601903d8e6e12a19659679c1032cf3ff22
mariarahel <mariarahel@cli.dysnomia.airforce>
2026-06-28 16:22:48 -0700

    feat: simplify Wayland widget to show only clock at bottom-left and add click-through dock window hints

--- scripts/dysnomia_overlay.c
+++ scripts/dysnomia_overlay.c
@@ -2,7 +2,6 @@
 #include <time.h>
 #include <stdint.h>
 #include <stdio.h>
-#include <string.h>
 
 #define DYSNOMIA_ZERO 638403877000000000ULL
 #define TICKS_PER_DAY (86400ULL * 10000000ULL)
@@ -11,11 +10,9 @@
 #define TICKS_PER_SECOND (TICKS_PER_MINUTE / 34ULL)
 #define NET_UNIX_EPOCH_TICKS 621355968000000000ULL
 
-GtkWidget *label_left;
-GtkWidget *label_right;
+GtkWidget *label_time;
 
-void get_dysnomia_time(char *out_day, char *out_time) {
-    // Current time in .NET ticks
+void get_dysnomia_time(char *out_time) {
     uint64_t current_ticks = (uint64_t)time(NULL) * 10000000ULL + NET_UNIX_EPOCH_TICKS;
     int64_t ticks = (int64_t)current_ticks - DYSNOMIA_ZERO;
     if (ticks < 0) ticks = 0;
@@ -25,32 +22,33 @@ void get_dysnomia_time(char *out_day, char *out_time) {
     int minute = ((ticks % TICKS_PER_DAY) % TICKS_PER_HOUR) / TICKS_PER_MINUTE;
     int second = (((ticks % TICKS_PER_DAY) % TICKS_PER_HOUR) % TICKS_PER_MINUTE) / TICKS_PER_SECOND;
 
-    sprintf(out_day, "Day %04d", day);
+    // Output only the [dXXXX/HHMMSS] format without the prefix space
     sprintf(out_time, "[d%04d/%02d%02d%02d]", day, hour, minute, second);
 }
 
 static gboolean update_time(gpointer data) {
-    char day_str[64];
     char time_str[64];
-    get_dysnomia_time(day_str, time_str);
+    get_dysnomia_time(time_str);
 
-    // Update labels with markup for hot-pink styling
-    char markup_left[256];
-    char markup_right[256];
-    sprintf(markup_left, "<span font_desc='Consolas Bold 11' foreground='DeepPink'>%s</span>", day_str);
-    sprintf(markup_right, "<span font_desc='Consolas Bold 11' foreground='DeepPink'>%s</span>", time_str);
-
-    gtk_label_set_markup(GTK_LABEL(label_left), markup_left);
-    gtk_label_set_markup(GTK_LABEL(label_right), markup_right);
+    char markup[256];
+    sprintf(markup, "<span font_desc='Consolas Bold 11' foreground='DeepPink'>%s</span>", time_str);
+    gtk_label_set_markup(GTK_LABEL(label_time), markup);
     return TRUE;
 }
 
 static gboolean draw_callback(GtkWidget *widget, cairo_t *cr, gpointer data) {
-    // Completely transparent window background
     cairo_set_source_rgba(cr, 0.0, 0.0, 0.0, 0.0);
     cairo_set_operator(cr, CAIRO_OPERATOR_SOURCE);
     cairo_paint(cr);
     return FALSE;
+}
+
+static void realize_callback(GtkWidget *widget, gpointer data) {
+    GdkWindow *gdk_win = gtk_widget_get_window(widget);
+    if (gdk_win) {
+        // Enable input events pass-through (click-through overlay)
+        gdk_window_set_pass_through(gdk_win, TRUE);
+    }
 }
 
 int main(int argc, char *argv[]) {
@@ -61,6 +59,10 @@ int main(int argc, char *argv[]) {
     gtk_window_set_decorated(GTK_WINDOW(window), FALSE);
     gtk_window_set_skip_taskbar_hint(GTK_WINDOW(window), TRUE);
     gtk_window_set_keep_above(GTK_WINDOW(window), TRUE);
+    
+    // Set as a Dock type window and refuse focus for desktop overlay behavior
+    gtk_window_set_type_hint(GTK_WINDOW(window), GDK_WINDOW_TYPE_HINT_DOCK);
+    gtk_window_set_accept_focus(GTK_WINDOW(window), FALSE);
 
     // Support transparency
     GdkScreen *screen = gtk_widget_get_screen(window);
@@ -69,30 +71,28 @@ int main(int argc, char *argv[]) {
         gtk_widget_set_visual(window, visual);
     }
     gtk_widget_set_app_paintable(window, TRUE);
+    
     g_signal_connect(G_OBJECT(window), "draw", G_CALLBACK(draw_callback), NULL);
+    g_signal_connect(G_OBJECT(window), "realize", G_CALLBACK(realize_callback), NULL);
 
     // Layout
     GtkWidget *grid = gtk_grid_new();
     gtk_widget_set_valign(grid, GTK_ALIGN_CENTER);
     gtk_container_add(GTK_CONTAINER(window), grid);
 
-    label_left = gtk_label_new(NULL);
-    gtk_widget_set_hexpand(label_left, TRUE);
-    gtk_widget_set_halign(label_left, GTK_ALIGN_START);
-    gtk_grid_attach(GTK_GRID(grid), label_left, 0, 0, 1, 1);
+    label_time = gtk_label_new(NULL);
+    // Align to the left (start) of the monitor
+    gtk_widget_set_hexpand(label_time, TRUE);
+    gtk_widget_set_halign(label_time, GTK_ALIGN_START);
+    // Margin of 16px to prevent touching the absolute left screen border
+    gtk_widget_set_margin_start(label_time, 16);
+    gtk_grid_attach(GTK_GRID(grid), label_time, 0, 0, 1, 1);
 
-    label_right = gtk_label_new(NULL);
-    gtk_widget_set_hexpand(label_right, TRUE);
-    gtk_widget_set_halign(label_right, GTK_ALIGN_END);
-    gtk_grid_attach(GTK_GRID(grid), label_right, 1, 0, 1, 1);
-
-    // Initial width spans screen, thin height at bottom
-    // Under Wayland, default size acts as size hint for shell overlays
-    gtk_window_set_default_size(GTK_WINDOW(window), 1280, 22);
+    // Size hint to stretch across screen at the bottom
+    gtk_window_set_default_size(GTK_WINDOW(window), 1920, 22);
 
     g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
 
-    // Schedule 111ms tick updates (approx. 9 Hz)
     update_time(NULL);
     g_timeout_add(111, update_time, NULL);
 
