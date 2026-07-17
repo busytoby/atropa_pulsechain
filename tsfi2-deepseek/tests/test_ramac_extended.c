a3991bd1459175a1d82a20016aedd0d52f33aec7
mariarahel <mariarahel@cli.dysnomia.airforce>
2026-07-16 18:50:49 -0700

    Deploy and verify variable shadowing .algol61 program in domain/std

--- tsfi2-deepseek/tests/test_ramac_extended.c
+++ tsfi2-deepseek/tests/test_ramac_extended.c
@@ -404,10 +404,11 @@ int main(void) {
         "../solidity/dysnomia/domain/std/conditional_expression.algol61",
         "../solidity/dysnomia/domain/std/procedure_param.algol61",
         "../solidity/dysnomia/domain/std/computed_goto.algol61",
-        "../solidity/dysnomia/domain/std/designational_goto.algol61"
+        "../solidity/dysnomia/domain/std/designational_goto.algol61",
+        "../solidity/dysnomia/domain/std/variable_shadowing.algol61"
     };
     
-    for (int i = 0; i < 6; i++) {
+    for (int i = 0; i < 7; i++) {
         FILE *f = fopen(files[i], "r");
         assert(f != NULL);
         
