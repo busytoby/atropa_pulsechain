d00b78fd8d5bcf8b84a4ac9b8eb9484e2ec82163
mariarahel <mariarahel@cli.dysnomia.airforce>
2026-07-25 12:23:57 -0700

    Update PackQing to calculate coordinate differences relative to parent QING

--- solidity/dysnomia/domain/dan/03d_pack_qing.sol
+++ solidity/dysnomia/domain/dan/03d_pack_qing.sol
@@ -5,18 +5,24 @@ import "./03_qing.sol";
 contract PackQing is QING {
     string public constant PackSubType = "PackQing";
 
+    // Parent QING contract reference
+    address public ParentQing;
+
     // Dynamic array of packaged QING coordinate words
     uint256[] public PackedWords;
 
-    constructor(uint256 Luo, address Integrative, address ChoAddress) QING(Luo, Integrative, ChoAddress) {
-        /* Constructor inherits and runs initializations */
+    constructor(uint256 Luo, address Integrative, address ChoAddress, address ParentQingAddress) QING(Luo, Integrative, ChoAddress) {
+        ParentQing = ParentQingAddress;
     }
 
-    // Packages many QING coordinate words up together at this pack qing
+    // Packages many QING coordinate words up together at this pack qing relative to the parent qing
     function PackQingWords(address[] memory qingAddresses) public onlyOwners {
+        uint256 parentWaat = QING(ParentQing).Waat();
         for (uint256 i = 0; i < qingAddresses.length; i++) {
             uint256 targetWaat = QING(qingAddresses[i]).Waat();
-            PackedWords.push(targetWaat);
+            // Establish the word relative to the parent qing using modular difference
+            uint256 relativeWaat = (targetWaat + MotzkinPrime - (parentWaat % MotzkinPrime)) % MotzkinPrime;
+            PackedWords.push(relativeWaat);
         }
     }
 
