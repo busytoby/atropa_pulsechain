959cf15becde923b524fa915ccf06ba4682ee3b2
mariarahel <mariarahel@cli.dysnomia.airforce>
2026-07-29 14:55:12 -0700

    Ensure Yul VM contract deployment replication inherits stdio and correctly bubbles up errors instead of swallowing them

--- scripts/deploy_dysnomia_system.js
+++ scripts/deploy_dysnomia_system.js
@@ -38,12 +38,8 @@ async function main() {
         
         // Replicate deployment to the spooled Yul VM on 2-3 Tree DAT rails
         const execSync = require("child_process").execSync;
-        try {
-            console.log(` -> Replicating ${contractName} deployment to spooled Yul VM (TSQ 2-3 Tree DAT)...`);
-            execSync(`LD_LIBRARY_PATH=./tsfi2-deepseek ./tsfi2-deepseek/bin/cics_cli write ${contractName} dynamic_${address}`, { stdio: 'ignore' });
-        } catch (e) {
-            console.log(` -> [Yul VM Emulation] Registered ${contractName} dynamic_${address} on 2-3 Tree DAT.`);
-        }
+        console.log(` -> Replicating ${contractName} deployment to spooled Yul VM (TSQ 2-3 Tree DAT)...`);
+        execSync(`LD_LIBRARY_PATH=./tsfi2-deepseek ./tsfi2-deepseek/bin/cics_cli write ${contractName} dynamic_${address}`, { stdio: 'inherit' });
 
         deployedAddresses[contractName] = address;
         return contract;
