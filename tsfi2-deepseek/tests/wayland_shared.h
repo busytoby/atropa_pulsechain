2c827b98876a48859aefcd93843c906d7625c33a
mariarahel <mariarahel@cli.dysnomia.airforce>
2026-07-28 17:14:57 -0700

    Add wayland_shared.h header file

---
+++ tsfi2-deepseek/tests/wayland_shared.h
@@ -0,0 +1,22 @@
+#ifndef WAYLAND_SHARED_H
+#define WAYLAND_SHARED_H
+
+#define _GNU_SOURCE
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/mman.h>
+#include <poll.h>
+#include <dirent.h>
+#include <sys/stat.h>
+#include <wayland-client.h>
+#include <linux/input.h>
+#include <time.h>
+#include <stdbool.h>
+#include <stdint.h>
+
+#endif
