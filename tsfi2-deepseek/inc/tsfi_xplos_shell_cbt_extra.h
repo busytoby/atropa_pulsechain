f7ad55b80b434a694961d4acf21e0b41d72da317
mariarahel <mariarahel@cli.dysnomia.airforce>
2026-07-29 09:03:15 -0700

    Delete obsolete monolithic and diagnostic CBT shell files

--- tsfi2-deepseek/inc/tsfi_xplos_shell_cbt_extra.h
+++
@@ -1,9 +0,0 @@
-#ifndef TSFI_XPLOS_SHELL_CBT_EXTRA_H
-#define TSFI_XPLOS_SHELL_CBT_EXTRA_H
-
-#include <stdbool.h>
-
-/* Evaluates CBT extra commands for Usenet, REXX interpreter, JCL block executor, and ISPF menu */
-bool tsfi_xplos_shell_cbt_extra(const char *cmd);
-
-#endif /* TSFI_XPLOS_SHELL_CBT_EXTRA_H */
