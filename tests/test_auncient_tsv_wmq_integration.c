9b2bff3db13c01832474e7c74b6a9d7d5fdab05c
mariarahel <mariarahel@cli.dysnomia.airforce>
2026-08-04 09:00:00 -0700

    Update test_auncient_tsv_wmq_integration.c with peer verification call

--- tests/test_auncient_tsv_wmq_integration.c
+++ tests/test_auncient_tsv_wmq_integration.c
@@ -3,6 +3,7 @@ int main() {
     __builtin_wmq_auth_idx(3);
     __builtin_wmq_timeout_idx(4);
     __builtin_wmq_key_idx(5);
+    __builtin_wmq_peer_idx(6);
     __builtin_wmq_disconnect();
     return 42;
 }
